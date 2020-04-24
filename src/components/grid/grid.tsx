89864097747f25e50f65ee5d3340a26c44de2266
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2020-04-24 10:58:43 +0200

    fix: use correct grid gap from design

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -4,7 +4,7 @@ import { up } from '../breakpoint/breakpoint';
 type GridItemSize = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 const GRID_GAP_DESKTOP: CSSProp = '24px';
-const GRID_GAP_MOBILE: CSSProp = '12px';
+const GRID_GAP_MOBILE: CSSProp = '16px';
 
 interface GridProps {
   disableGap?: boolean;
