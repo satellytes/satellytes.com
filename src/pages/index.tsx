df6dfd60fd1110608c4ea1f88f39865711e60746
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2020-05-08 12:42:32 +0200

    feat: make content of block-teaser more dynamic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { PageTitle, SectionTitle } from '../components/typography/typography';
 import styled from 'styled-components';
 import { up } from '../components/breakpoint/breakpoint';
 import { BlockTeaser } from '../components/teasers/block-teaser';
+import { Text } from '../components/typography/typography';
 
 const HomePageTitle = styled(PageTitle)`
   margin-top: 108px;
@@ -51,24 +52,54 @@ const IndexPage: React.FC = () => (
         <BlockTeaser
           preTitle="Services"
           title="Full Stack"
-          text="Satellytes ist eine Digital-Agentur, die um große Unternehmen kreist und ihnen bei der Transformation und Optimierung digitaler Services und Interfaces hilft. Wir bieten „Full Stack“ an, also den gesamten Prozess von Ideation bis zur Implementierung des letzten performanten Funnels und der letzten Zeile wunderschönen Codes."
           link="All Services >"
-        />
+          linkTo="/services"
+        >
+          <Text>
+            Satellytes ist eine Digital-Agentur, die um große Unternehmen kreist
+            und ihnen bei der Transformation und Optimierung digitaler Services
+            und Interfaces hilft. Wir bieten „Full Stack“ an, also den gesamten
+            Prozess von Ideation bis zur Implementierung des letzten
+            performanten Funnels und der letzten Zeile wunderschönen Codes.
+          </Text>
+        </BlockTeaser>
         <HomePageSubTitle>
           We are into relationships, not one-night-stands.
         </HomePageSubTitle>
         <BlockTeaser
           preTitle="Clients"
           title="Long term projects not fire & forget"
-          text="We are showing clients not projects since we are aiming for long term relationships.
-We are really good at creating design systems for complex web applications and self-testing, high performance pattern libraries."
           splitView
-        />
+        >
+          <Text>
+            We are showing clients not projects since we are aiming for long
+            term relationships.
+          </Text>
+          <Text>
+            We are really good at creating design systems for complex web
+            applications and self-testing, high performance pattern libraries.
+          </Text>
+        </BlockTeaser>
         <ClientList>List</ClientList>
         <HomePageSubTitle>
           We are on your side - and the customer&lsquo;s.
         </HomePageSubTitle>
-        <p>Card</p>
+        <BlockTeaser
+          preTitle="About"
+          title="Passionate geeks with high ambitions"
+          link="About us"
+          linkTo="/about"
+          splitView
+        >
+          <Text>
+            We are showing clients not projects since we are aiming for long
+            term relationships.
+          </Text>
+          <Text>
+            We are really good at creating design systems for complex web
+            applications and self-testing, high performance pattern libraries.
+          </Text>
+        </BlockTeaser>
       </GridItem>
     </Grid>
   </Layout>
