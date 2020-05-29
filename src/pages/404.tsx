318bca62317de5c9f8ff7db6fc10d5bfe4b5a0b9
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2020-05-29 15:28:41 +0200

    feat: align 404 page more to the design

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 
 import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
-import { PageTitle } from '../components/typography/typography';
+import { SubTitle } from '../components/typography/typography';
 import { Grid, GridItem } from '../components/grid/grid';
+import styled from 'styled-components';
+import { up } from '../components/breakpoint/breakpoint';
+
+const NotFoundTitle = styled(SubTitle)`
+  ${up('md')} {
+    margin-top: 220px;
+  }
+`;
 
 const NotFoundPage: React.FC = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <Grid>
-      <GridItem>
-        <PageTitle>Oh sorry! Our Satellite took the wrong direction.</PageTitle>
+    <Grid center>
+      <GridItem xs={0} md={2} />
+      <GridItem xs={12} md={8}>
+        <NotFoundTitle>
+          Oh sorry! Our Satellite took the wrong direction.
+        </NotFoundTitle>
       </GridItem>
     </Grid>
   </Layout>
