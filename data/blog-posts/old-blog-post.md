057a92ac9bb91c948e680911a6885ca43f55f42a
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2020-05-29 14:18:25 +0200

    feat: list all blog posts on the blog post page

---
+++ data/blog-posts/old-blog-post.md
@@ -0,0 +1,8 @@
+---
+path: "/blog/old-blog-post"
+date: "2019-01-04"
+title: "So old"
+---
+
+# This is a old blog post
+
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -4,9 +4,44 @@ import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
 import { PageTitle } from '../components/typography/typography';
 import { Grid, GridItem } from '../components/grid/grid';
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
+
+interface AllBlogPostsQuery {
+  allMarkdownRemark: {
+    nodes: {
+      id: string;
+      frontmatter: {
+        date: string;
+        path: string;
+        title: string;
+      };
+    }[];
+  };
+}
 
 const BlogPage: React.FC = () => {
+  const data = useStaticQuery<AllBlogPostsQuery>(graphql`
+    query getBlogPosts {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          id
+          frontmatter {
+            date
+            path
+            title
+          }
+        }
+      }
+    }
+  `);
+
+  const blogPosts = data.allMarkdownRemark.nodes.map((post) => {
+    return {
+      id: post.id,
+      ...post.frontmatter,
+    };
+  });
+
   return (
     <Layout>
       <SEO title="Blog" />
@@ -15,7 +50,15 @@ const BlogPage: React.FC = () => {
           <PageTitle>Blog</PageTitle>
         </GridItem>
         <GridItem>
-          <Link to="/blog/how-to-do-a-blog-post">Blog Post</Link>
+          <ul>
+            {blogPosts.map((post) => (
+              <li key={post.id}>
+                <Link to={post.path}>
+                  {post.title} ({post.date})
+                </Link>
+              </li>
+            ))}
+          </ul>
         </GridItem>
       </Grid>
     </Layout>
