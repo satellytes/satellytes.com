2233f00d56fe78c94adf941baffb01bdab48f393
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2020-07-10 12:41:55 +0200

    feat: add first draft of contact page

---
+++ src/components/form/contact.tsx
@@ -0,0 +1,168 @@
+import React, { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import styled from 'styled-components';
+import { Text } from '../typography/typography';
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 19px 16px;
+
+  font-size: 16px;
+  line-height: 110%;
+
+  border-radius: 4px;
+  border: 0;
+  background: #f5f6f7;
+
+  &:not(:last-of-type) {
+    margin-right: 24px;
+  }
+`;
+
+const TextArea = styled.textarea`
+  height: 190px;
+  width: 100%;
+  padding: 19px 16px;
+  margin-top: 24px;
+  margin-bottom: 40px;
+
+  font-size: 16px;
+  line-height: 110%;
+
+  border-radius: 4px;
+  border: 0;
+  background: #f5f6f7;
+`;
+
+const SendButton = styled.button`
+  cursor: pointer;
+  padding: 13px 18px;
+
+  font-size: 20px;
+  line-height: 110%;
+
+  color: #ffffff;
+  background: #668cff;
+  border-radius: 28px;
+  border: 0;
+
+  &:disabled {
+    cursor: auto;
+    opacity: 0.7;
+  }
+`;
+
+const RequestStatusMessage = styled(Text)`
+  display: inline-block;
+  margin-left: 24px;
+`;
+
+type RequestStatus = 'pending' | 'success' | 'error';
+
+export const ContactForm: React.FC = () => {
+  const initialSate = { name: '', email: '', message: '' };
+  const [state, setState] = useState(initialSate);
+  const [requestStatus, setRequestStatus] = useState<RequestStatus>('pending');
+
+  const hasValidInput = (): boolean => {
+    return Boolean(
+      state.name &&
+        state.name.length > 0 &&
+        state.email &&
+        state.email.length > 0 &&
+        state.message &&
+        state.message.length > 0,
+    );
+  };
+
+  const handleSubmit: FormEventHandler = (e) => {
+    const encode = (data: any): string => {
+      return Object.keys(data)
+        .map(
+          (key) =>
+            encodeURIComponent(key) + '=' + encodeURIComponent(data[key]),
+        )
+        .join('&');
+    };
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...state }),
+    })
+      .then(() => {
+        setRequestStatus('success');
+        setState(initialSate);
+      })
+      .catch((error) => {
+        console.error(error);
+        setRequestStatus('error');
+      });
+
+    e.preventDefault();
+  };
+
+  const handleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      onSubmit={handleSubmit}
+    >
+      <InputContainer>
+        <Input
+          placeholder="Your name"
+          type="text"
+          name="name"
+          value={state.name}
+          onChange={handleChange}
+        />
+        <Input
+          placeholder="Your email"
+          type="email"
+          name="email"
+          value={state.email}
+          onChange={handleChange}
+        />
+      </InputContainer>
+      <TextArea
+        placeholder="Your message to us"
+        name="message"
+        value={state.message}
+        onChange={handleChange}
+      />
+      <div>
+        <SendButton
+          disabled={!hasValidInput()}
+          type="submit"
+          title={
+            hasValidInput()
+              ? 'Click to send us your message'
+              : 'Please fill in all information'
+          }
+        >
+          Send &#8594;
+        </SendButton>
+        {requestStatus === 'success' && (
+          <RequestStatusMessage>Thanks for your message!</RequestStatusMessage>
+        )}
+        {requestStatus === 'error' && (
+          <RequestStatusMessage>
+            Something went wrong, please try again later.
+          </RequestStatusMessage>
+        )}
+      </div>
+    </form>
+  );
+};
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,16 +2,68 @@ import React from 'react';
 
 import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
-import { PageTitle } from '../components/typography/typography';
+import { SubTitle, Text, TextLink } from '../components/typography/typography';
 import { Grid, GridItem } from '../components/grid/grid';
+import { ContactForm } from '../components/form/contact';
+import styled from 'styled-components';
+import { up } from '../components/breakpoint/breakpoint';
+
+const ContactSubTitle = styled(SubTitle)`
+  margin-top: 80px;
+
+  ${up('md')} {
+    margin-top: 160px;
+  }
+`;
 
 const ContactPage: React.FC = () => {
   return (
     <Layout>
       <SEO title="Contact" />
-      <Grid>
-        <GridItem>
-          <PageTitle>Contact</PageTitle>
+      <Grid center>
+        <GridItem xs={0} md={2} />
+        <GridItem xs={12} md={8}>
+          <SubTitle>Adresse</SubTitle>
+          <div>
+            <Text>
+              <b>Satellytes Digital Consulting GmbH</b>
+              <br />
+              Alpenplatz 3
+              <br />
+              81541 München
+            </Text>
+            <TextLink
+              as="a"
+              target="_blank"
+              href="https://goo.gl/maps/sP2K4NUzqTS1zwVs9"
+            >
+              Google Maps &gt;
+            </TextLink>
+            <ContactSubTitle>Events</ContactSubTitle>
+            <Text>
+              Wir lieben es Neues zu entdecken und neue Kontakte zu knüpfen.
+              Dort sind wir demnächst:
+            </Text>
+            <Text>Aktuell sind leider keine Events geplant.</Text>
+            <Text>
+              Sie haben ein Event für uns, das wir nicht verpassen sollten? Oder
+              Sie hätten gerne, dass wir auf Ihrem einen Vortrag halten? Machen
+              wir gerne.
+            </Text>
+            <TextLink as="a" href="mailto:ping@satellytes.com">
+              Schreiben Sie uns &gt;
+            </TextLink>
+            <ContactSubTitle>E-Mail</ContactSubTitle>
+            <Text style={{ marginBottom: '40px' }}>
+              Nutzen Sie unser Kontaktformular oder schreiben Sie uns eine
+              E-Mail an{' '}
+              <TextLink as="a" href="mailto:ping@satellytes.com">
+                ping@satellytes.com
+              </TextLink>
+              .
+            </Text>
+            <ContactForm />
+          </div>
         </GridItem>
       </Grid>
     </Layout>
