9c378ec96fc4a0771c5fcaffdd983ff355b6d7e0
Kateryna Bugaieva <35267034+katerynabugaieva@users.noreply.github.com>
2020-09-02 12:06:42 +0200

    Implement correct states for the contact form (#41)
    * feat: add error messages
    
    * fix: fix onsubmit handler
    
    * feat: add sent button
    
    * :package: add react-hook-form
    
    * chore: refactor buttons style
    
    * chore: update office address
    
    * chore: update invalid fields styles
    
    * style: add button icons and some styles
    
    * style: update sent button backgroung color
    
    * updated style into nested components
    
    * chore: PR-updates

--- src/components/form/contact.tsx
+++ src/components/form/contact.tsx
@@ -1,7 +1,13 @@
 import React, { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
-import { Text } from '../typography/typography';
 import { up } from '../breakpoint/breakpoint';
+import { CheckmarkIcon } from '../icons/buttons-icons/checkmark';
+import { RightArrowIcon } from '../icons/buttons-icons/right-arrow';
+import { Text } from '../typography/typography';
+
+//https://www.codegrepper.com/code-examples/basic/form+validation+in+gatsby
+const IS_EMAIL_REGEX = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const InputContainer = styled.div`
   display: flex;
@@ -13,7 +19,24 @@ const InputContainer = styled.div`
   }
 `;
 
-const Input = styled.input`
+const InputWrapper = styled.div`
+  width: 100%;
+  margin-bottom: 24px;
+
+  &:not(:last-of-type) {
+    margin-right: 24px;
+
+    ${up('md')} {
+      margin-right: 24px;
+    }
+  }
+`;
+
+interface ValidationProps {
+  hasError?: boolean;
+}
+
+const Input = styled.input<ValidationProps>`
   width: 100%;
   padding: 19px 16px;
 
@@ -25,7 +48,6 @@ const Input = styled.input`
   background: #f5f6f7;
 
   &:not(:last-of-type) {
-    margin-bottom: 24px;
     margin-right: 0;
 
     ${up('md')} {
@@ -33,14 +55,23 @@ const Input = styled.input`
       margin-bottom: 0;
     }
   }
+
+  ${({ hasError }) =>
+    hasError &&
+    `
+    background: #f8cdd5;
+    color: #dc052d;
+    ::placeholder {
+      color: #dc052d;
+      opacity: 1;
+    }
+  `}
 `;
 
-const TextArea = styled.textarea`
+const TextArea = styled.textarea<ValidationProps>`
   height: 190px;
   width: 100%;
   padding: 19px 16px;
-  margin-top: 24px;
-  margin-bottom: 40px;
 
   font-size: 16px;
   line-height: 110%;
@@ -50,27 +81,62 @@ const TextArea = styled.textarea`
   background: #f5f6f7;
 
   resize: vertical;
+
+  ${({ hasError }) =>
+    hasError &&
+    `
+  background: #f8cdd5;
+  ::placeholder {
+    color: #dc052d;
+    opacity: 1;
+  }
+`}
 `;
 
-const SendButton = styled.button`
+const Button = styled.button`
+  margin-top: 40px;
   cursor: pointer;
   padding: 13px 18px;
 
   font-size: 20px;
+  font-weight: bold;
+  text-align: left;
   line-height: 110%;
 
-  color: #ffffff;
-  background: #668cff;
   border-radius: 28px;
   border: 0;
+  width: 147px;
+`;
 
-  &:disabled {
-    cursor: auto;
-    opacity: 0.7;
-  }
+const ButtonText = styled.span`
+  margin-right: 40px;
+`;
+
+const SendButton = styled(Button)`
+  color: #ffffff;
+  background: #668cff;
+`;
+
+const SentButton = styled(Button)`
+  color: #202840;
+  background: #75f0c7;
 `;
 
 const RequestStatusMessage = styled(Text)`
+  display: inline-block;
+  margin-left: 24px;
+  color: #668cff;
+  font-size: 14px;
+`;
+
+const ErrorMessage = styled.p`
+  color: #dc052d;
+  font-size: 14px;
+  margin-top: 8px;
+  margin-bottom: 0;
+`;
+
+const ErrorMessageSend = styled(ErrorMessage)`
   display: inline-block;
   margin-left: 24px;
 `;
@@ -82,20 +148,12 @@ export const ContactForm: React.FC = () => {
   const [formData, setFormData] = useState(initialFormData);
   const [requestStatus, setRequestStatus] = useState<RequestStatus>('pending');
 
-  const hasValidInput = (): boolean => {
-    return Boolean(
-      formData.name &&
-        formData.name.length > 0 &&
-        formData.email &&
-        formData.email.length > 0 &&
-        formData.message &&
-        formData.message.length > 0,
-    );
-  };
+  const { register, errors, handleSubmit } = useForm();
 
-  const handleSubmit: FormEventHandler = (e) => {
-    // taken from the Netlify Blog:
+  const onSubmit: FormEventHandler = () => {
+    // partialy taken from the Netlify Blog:
     // - https://www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/
+
     const encodeForNetlify = (data: any): string => {
       return Object.keys(data)
         .map(
@@ -118,59 +176,88 @@ export const ContactForm: React.FC = () => {
         console.error(error);
         setRequestStatus('error');
       });
-
-    e.preventDefault();
   };
 
   const handleInputChange: ChangeEventHandler<
     HTMLInputElement | HTMLTextAreaElement
   > = (e) => {
+    setRequestStatus('pending');
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-
   return (
     <form
       name="contact"
       method="POST"
       data-netlify="true"
-      onSubmit={handleSubmit}
+      data-netlify-honeypot="bot-field"
+      onSubmit={handleSubmit(onSubmit)}
     >
       <InputContainer>
-        <Input
-          placeholder="Your name"
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleInputChange}
-        />
-        <Input
-          placeholder="Your email"
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleInputChange}
-        />
+        <InputWrapper>
+          <Input
+            placeholder="Your name"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            ref={register({ required: true })}
+            hasError={errors.name}
+          />
+
+          {errors.name && (
+            <ErrorMessage>
+              <strong>Name: </strong>
+              <span>Bitte geben Sie einen Namen ein</span>
+            </ErrorMessage>
+          )}
+        </InputWrapper>
+        <InputWrapper>
+          <Input
+            placeholder="Your email"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            ref={register({
+              required: true,
+              pattern: IS_EMAIL_REGEX,
+            })}
+            hasError={errors.email}
+          />
+
+          {errors.email && (
+            <ErrorMessage>
+              <strong>E-Mail-Adresse: </strong>
+              <span>Bei dieser Adresse scheint etwas nicht zu stimmen</span>
+            </ErrorMessage>
+          )}
+        </InputWrapper>
       </InputContainer>
       <TextArea
         placeholder="Your message to us"
         name="message"
         value={formData.message}
         onChange={handleInputChange}
+        ref={register({ required: true })}
+        hasError={errors.message}
       />
+
+      {errors.message && (
+        <ErrorMessage>Diese Nachricht ist ein bisschen zu kurz</ErrorMessage>
+      )}
       <div>
-        <SendButton
-          disabled={!hasValidInput()}
-          type="submit"
-          title={
-            hasValidInput()
-              ? 'Click to send us your message'
-              : 'Please fill in all information'
-          }
-        >
-          Send &#8594;
-        </SendButton>
+        {requestStatus === 'pending' && (
+          <SendButton type="submit">
+            <ButtonText>Send</ButtonText> <RightArrowIcon />
+          </SendButton>
+        )}
+        {(errors.name || errors.email || errors.message) && (
+          <ErrorMessageSend>Bitte füllen Sie alle Felder aus</ErrorMessageSend>
+        )}
         {requestStatus === 'success' && (
-          <RequestStatusMessage>Thanks for your message!</RequestStatusMessage>
+          <SentButton type="button">
+            <ButtonText>Sent</ButtonText> <CheckmarkIcon />
+          </SentButton>
         )}
         {requestStatus === 'error' && (
           <RequestStatusMessage>
---
+++ src/components/icons/buttons-icons/right-arrow.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const RightArrowIcon: React.FC = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9 2L14 8L9 14"
+      stroke="white"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+    />
+    <path d="M14 8H2" stroke="white" strokeWidth="2.5" strokeLinecap="round" />
+  </svg>
+);
