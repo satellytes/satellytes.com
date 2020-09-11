a791f14d904f75a720adb7375ad0f52352d38f29
katerynabugaieva <sonnensmile@gmail.com>
2020-09-11 13:28:48 +0200

    style: mouseover nav social icon

--- src/components/icons/social/xing.tsx
+++ src/components/icons/social/xing.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { css } from '@emotion/core';
 
 interface XingProps {
   color?: string;
@@ -11,15 +12,16 @@ export const IconXing: React.FC<XingProps> = (props) => (
     viewBox="0 0 21 22"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    css={css`
+      transition: 0.3s;
+      fill: ${props.color ? props.color : '#202840'};
+      &:hover {
+        fill: ${props.color ? props.color : '#ffffff'};
+      }
+    `}
     {...props}
   >
-    <path
-      d="M20.5 0H15.3704L7.75 13.3746L12.6601 22H17.7897L12.8796 13.3746L20.5 0Z"
-      fill={props.color ? props.color : '#202840'}
-    />
-    <path
-      d="M5.7055 4.25H0.875766L3.67027 9.07541L0 15H4.82973L8.5 9.07541L5.7055 4.25Z"
-      fill={props.color ? props.color : '#202840'}
-    />
+    <path d="M20.5 0H15.3704L7.75 13.3746L12.6601 22H17.7897L12.8796 13.3746L20.5 0Z" />
+    <path d="M5.7055 4.25H0.875766L3.67027 9.07541L0 15H4.82973L8.5 9.07541L5.7055 4.25Z" />
   </svg>
 );
