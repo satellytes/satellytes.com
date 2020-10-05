6ed8ae7ac6ccb79637d827c14ae74a3c58923e13
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2020-10-05 14:14:46 +0200

    feat: give header background on scroll

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'gatsby';
 import { FluidObject } from 'gatsby-image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { up } from '../breakpoint/breakpoint';
 import { GRID_GAP_DESKTOP, GRID_GAP_MOBILE } from '../grid/grid';
@@ -16,6 +16,7 @@ const StyledHeader = styled.header<{ light: number; transparent: number }>`
   top: 0;
   width: 100%;
   z-index: 100;
+  transition: background-color 0.3s;
 
   background-color: ${(props) =>
     props.transparent === 1
@@ -93,12 +94,30 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = (props) => {
   const [isNavigationVisible, setIsNavigationVisible] = useState(false);
+  const [isTransparent, setIsTransparent] = useState<boolean>(
+    Boolean(props.heroImage),
+  );
+
+  useEffect(() => {
+    const onScroll = (): void => {
+      if (props.heroImage) {
+        if (window.scrollY !== 0) {
+          setIsTransparent(false);
+        } else {
+          setIsTransparent(true);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  });
 
   return (
     <HeroImage image={props.heroImage}>
       <StyledHeader
         light={props.light ? 1 : 0}
-        transparent={props.heroImage ? 1 : 0}
+        transparent={isTransparent ? 1 : 0}
       >
         <SiteTitle to={props.siteTitleUrl || '/'} light={props.light ? 1 : 0}>
           {props.siteTitle}
