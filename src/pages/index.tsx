299a8726b3102eb5a1faa645a4f43bf6e5217f43
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2020-11-04 18:32:06 +0100

    chore: use cloudinary https url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -82,6 +82,7 @@ const IndexPage: React.FC = () => {
         cloudName="satellytes"
         publicId="satellytes-website/sy-image-home_l5zkwg"
         format="webp"
+        secure={true}
       />
       <Layout isIndexPage={true}>
         <SEO title="Home" />
