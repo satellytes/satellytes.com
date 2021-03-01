a68880a5e0bff2bfef66e0a1ac8369138ac68b6e
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2021-03-01 10:43:28 +0100

    fix: use nicer close icon

--- src/components/icons/buttons-icons/close-burger-menu.tsx
+++ src/components/icons/buttons-icons/close-burger-menu.tsx
@@ -2,28 +2,28 @@ import React from 'react';
 
 export const CloseBurgerMenuIcon: React.FC = () => (
   <svg
-    width="20"
-    height="20"
-    viewBox="0 0 20 20"
-    fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
   >
     <rect
-      x="5.65674"
-      y="4.34326"
+      x="1.65723"
+      y="0.343262"
       width="14"
       height="2"
       rx="1"
-      transform="rotate(45 5.65674 4.34326)"
+      transform="rotate(45 1.65723 0.343262)"
       fill="white"
     />
     <rect
-      x="15.5562"
-      y="5.75732"
+      x="11.5566"
+      y="1.75732"
       width="14"
       height="2"
       rx="1"
-      transform="rotate(135 15.5562 5.75732)"
+      transform="rotate(135 11.5566 1.75732)"
       fill="white"
     />
   </svg>
