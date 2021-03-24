a9a8630ee588a75fac3d0421a96e76e828060249
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2021-03-24 18:30:09 +0100

    feat: add career pages from personio

--- data/pages/career.md
+++ data/pages/career.md
@@ -1,28 +1,3 @@
 Du hast noch nicht viel Berufserfahrung? Kein Problem. Denn alles, was du wissen musst, lernst Du bei uns. Du kannst
 schon alles? Dann findest Du bei Satellytes neue Herausforderungen und erfahrene Kollegen, mit denen Du weiter wachsen
 kannst. Schaue Dir unsere offenen Stellen an. Wir freuen uns auf Deine Bewerbung.
-
-Wir suchen derzeit neue Kollegen für folgende Stellen:
-
-### [Senior Frontend Engineer (m/w/d)](#senior-frontend-engineer)
-
-Wir suchen Dich und Deine Expertise in der Frontend-Entwicklung! Deine Erfahrung in Projekten reicht sechs Jahre oder
-länger zurück und Du bist es gewohnt im Teams zu arbeiten? JavaScript beherrscht Du fließend und Du hast tiefgreifende
-Erfahrung im Umgang mit einem einschlägigen Framework wie Angular, React, Vue oder Svelte? HTML & CSS sind für Dich
-nicht unangenehme Pflichten, sondern Fundament des Internets und Deiner Karriere?
-
-Du hörst niemals auf zu lernen, weil Du weißt, wie schnell sich das Web entwickelt? Testing ist für Dich ein Werkzeug,
-um Vertrauen in Deinen Code zu schaffen? Du hast über die Jahre Deine Fähigkeiten zur Priorisierung und Deinen
-Pragmatismus soweit geschärft, dass Du jedes unscharf formulierte Projekte zum Erfolg führen kannst?
-
-Wenn Du bis hierhin ein paar mal entspannt genickt hast und jetzt gern mehr wüsstest, dann bist Du genau richtig bei uns.
-
-Bewirb Dich am besten gleich per E-Mail an [career@satellytes.com](mailto:career@satellytes.com)
-
-### [Initiativbewerbung](#initiativbewerbung)
-
-Du glaubst, wir haben eine Stelle vergessen und Du würdest perfekt auf diesen Blindspot passen, um gemeinsam mit
-Satellytes die Zukunft zu gestalten? Dann schreib uns, wir sind neugierig.
-
-Schreib uns eine Mail an [career@satellytes.com](mailto:career@satellytes.com) mit Deinem CV und welche Rolle Du dir
-vorstellst.
---
+++ src/templates/career.tsx
@@ -0,0 +1,122 @@
+import React from 'react';
+
+import Layout from '../components/layout/layout';
+import SEO from '../components/seo';
+import {
+  LargeText,
+  PageTitle,
+  SectionTitle,
+  Text,
+  TextLink,
+} from '../components/typography/typography';
+import { Grid, GridItem } from '../components/grid/grid';
+import { Markdown } from '../components/markdown/markdown';
+import { graphql, useStaticQuery } from 'gatsby';
+import { JobCard } from '../components/cards/job-card';
+import styled from 'styled-components';
+import { up } from '../components/breakpoint/breakpoint';
+
+const PositionsTitle = styled(SectionTitle)`
+  font-size: 24px;
+  margin: 80px 0 24px 0;
+
+  ${up('md')} {
+    font-size: 32px;
+    margin: 120px 0 32px 0;
+  }
+`;
+
+const InfoText = styled(Text)`
+  font-size: 18px;
+  a {
+    font-size: 18px;
+  }
+
+  margin-top: 8px;
+`;
+
+interface CareerQuery {
+  markdownRemark: {
+    rawMarkdownBody: string;
+  };
+}
+
+interface PersonioJobNameValue {
+  name: string;
+  value: string;
+}
+
+export interface PersonioJobPosition {
+  id: number;
+  office: string;
+  department: string;
+  name: string;
+  jobDescriptions: {
+    jobDescription: PersonioJobNameValue[];
+  };
+  employmentType: string;
+  seniority: string;
+  schedule: string;
+  // comma separated
+  keywords: string;
+
+  satellytesPath: string;
+  satellytesShortDescription: string;
+}
+
+interface CareerPageProps {
+  pageContext: {
+    positions: PersonioJobPosition[];
+  };
+}
+
+const CareerPage = ({ pageContext }: CareerPageProps): JSX.Element => {
+  const data = useStaticQuery<CareerQuery>(graphql`
+    query {
+      markdownRemark(fileAbsolutePath: { regex: "/(pages/career)/" }) {
+        rawMarkdownBody
+      }
+    }
+  `);
+
+  return (
+    <Layout>
+      <SEO
+        title="Karriere | Satellytes"
+        description="Wir suchen Entwickler:innen aus Leidenschaft! Schaue Dir unsere offenen Stellen an. Wir freuen uns auf Deine Bewerbung."
+      />
+      <Grid>
+        <GridItem>
+          <PageTitle>Karriere</PageTitle>
+        </GridItem>
+        <GridItem xs={12} md={8}>
+          <LargeText as={'h2'}>
+            Wir suchen Entwickler:innen aus Leidenschaft!
+          </LargeText>
+          <Markdown>{data.markdownRemark.rawMarkdownBody}</Markdown>
+          <PositionsTitle>Opportunities</PositionsTitle>
+        </GridItem>
+        <GridItem xs={0} md={4} />
+        {pageContext.positions.map((position) => (
+          <GridItem xs={12} md={4} key={position.id}>
+            <JobCard
+              title={position.name}
+              text={position.satellytesShortDescription}
+              link={position.satellytesPath}
+            />
+          </GridItem>
+        ))}
+        <GridItem xs={12}>
+          <InfoText>
+            Oder schicke deine Bewerbung an:{' '}
+            <TextLink to="mailto:team@satellytes.com">
+              team@satellytes.com
+            </TextLink>
+          </InfoText>
+        </GridItem>
+      </Grid>
+    </Layout>
+  );
+};
+
+export default CareerPage;
