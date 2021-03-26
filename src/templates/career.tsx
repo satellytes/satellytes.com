0b5f6ecdc19bece6f550ac37bb99b5b45864a33e
Georgios Kaleadis (Satellytes) <georgios.kaleadis@satellytes.com>
2021-03-26 11:42:11 +0100

    chore: update career page

--- src/templates/career.tsx
+++ src/templates/career.tsx
@@ -94,7 +94,7 @@ const CareerPage = ({ pageContext }: CareerPageProps): JSX.Element => {
             Wir suchen Entwickler:innen aus Leidenschaft!
           </LargeText>
           <Markdown>{data.markdownRemark.rawMarkdownBody}</Markdown>
-          <PositionsTitle>Unsere offene Stellen</PositionsTitle>
+          <PositionsTitle>Unsere offenen Stellen</PositionsTitle>
         </GridItem>
         <GridItem xs={0} md={4} />
         {pageContext.positions.map((position) => (
