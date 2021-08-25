3e1d07d86da7f2e287d074b7732f1ab16bd4c4a7
Wiebke Freitag <wiebke.freitag@satellytes.com>
2021-08-25 14:59:58 +0200

    feat: added style for anchor links in markdown

--- src/components/markdown/custom-components.tsx
+++ src/components/markdown/custom-components.tsx
@@ -45,7 +45,8 @@ export const SubTitleCondensed = styled(SubTitle)`
   }
 `;
 
-export const SimpleLink = styled.a`
+export const SimpleLink = styled.a<{ href?: string }>`
+  position: relative;
   display: inline-block;
   color: ${theme.palette.text.link.default};
 
@@ -55,7 +56,26 @@ export const SimpleLink = styled.a`
 
   &:hover {
     color: ${theme.palette.text.link.hover};
-    text-decoration: underline;
+    ${({ href }) => {
+      if (href && href.startsWith('#')) {
+        // style for anchor link
+        return `
+           &::before {
+            content: url("data:image/svg+xml; utf8, <svg height='16' width='16' xmlns='http://www.w3.org/2000/svg'>
+                        <path fill-rule='evenodd' fill='white' opacity='0.4' d='M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z'/>
+                      </svg>");
+            display: block;
+            position: absolute;
+            height: 0px;
+            width: 16px;
+            top: -4px;
+            left: -24px;
+          }
+        `;
+      } else {
+        return `text-decoration: underline;`;
+      }
+    }}
   }
 `;
 
