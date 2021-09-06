9d080e74e3bc0d9a5742a5a7d00afdfdcfc6db0f
Georgios Kaleadis (Satellytes) <georgios.kaleadis@satellytes.com>
2021-09-06 12:39:47 +0200

    chore: provide distinct anchored link component

--- src/components/markdown/custom-components.tsx
+++ src/components/markdown/custom-components.tsx
@@ -10,6 +10,7 @@ import { rgba } from 'polished';
 import React from 'react';
 import { up } from '../breakpoint/breakpoint';
 import { theme } from '../layout/theme';
+import { Anchor } from '../icons/anchor';
 
 /**
  * Override markdown generated html content with custom React components (for us mostly to pass in custom styling)
@@ -55,28 +56,40 @@ export const SimpleLink = styled.a<{ href?: string }>`
   text-decoration: none;
 
   &:hover {
-    color: ${theme.palette.text.link.hover};
-    ${({ href }) => {
-      if (href && href.startsWith('#')) {
-        // style for anchor link
-        return `
-           &::before {
-            content: url("data:image/svg+xml; utf8, <svg height='16' width='16' xmlns='http://www.w3.org/2000/svg'>
-                        <path fill-rule='evenodd' fill='white' opacity='0.4' d='M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z'/>
-                      </svg>");
-            display: block;
-            position: absolute;
-            height: 0px;
-            width: 16px;
-            top: -4px;
-            left: -24px;
-          }
-        `;
-      } else {
-        return `text-decoration: underline;`;
-      }
-    }}
+    text-decoration: underline;
   }
+`;
+
+const AnchorIconStyled = styled(Anchor)`
+  display: inline-block;
+  position: absolute;
+  width: 0.5em;
+  height: 0.5em;
+  transform: translate(-0.75em, 0.5em);
+`;
+
+const AnchoredLink = (props) => {
+  return (
+    <SimpleLink {...props}>
+      <AnchorIconStyled />
+      {props.children}
+    </SimpleLink>
+  );
+};
+
+const AnchoredLinkStyled = styled(AnchoredLink)`
+  position: relative;
+  ${AnchorIconStyled} {
+     visibility: hidden;
+  }
+  
+  &:hover {
+    text-decoration: none;
+    ${AnchorIconStyled} {
+      visibility: visible;
+    }
+  }
+}
 `;
 
 const Blockquote = styled.blockquote`
@@ -138,6 +151,12 @@ const customSatellytesComponents = {
     return <DetailsStyled {...props}>{props.children}</DetailsStyled>;
   },
   a(props) {
+    if (props.href && props.href?.startsWith('#')) {
+      return (
+        <AnchoredLinkStyled {...props}>{props.children}</AnchoredLinkStyled>
+      );
+    }
+
     return <SimpleLink {...props}>{props.children}</SimpleLink>;
   },
   // define our lovely headlines from h1 to h6
