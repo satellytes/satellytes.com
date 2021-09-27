441db0499678cf08783151e9a853b93f123033c9
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2021-09-27 08:43:02 +0200

    fix: use semantically correct html for the language switch

--- src/components/language-switch/language-switch.tsx
+++ src/components/language-switch/language-switch.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from '../links/links';
 
+const UnstyledListItem = styled.li`
+  list-style-type: none;
+  display: inline;
+`;
+
 const StyledLanguageLink = styled(Link)<{ selected?: boolean }>`
-  margin: 1px 0px 1px 12px;
+  margin: 1px 0 1px 12px;
   font-weight: bold;
   font-size: 14px;
   line-height: 110%;
@@ -18,26 +23,30 @@ export const LanguageSwitch = ({
   translation,
   className = 'language-switch',
 }) => {
-  const { languages, language, originalPath } = useI18next();
+  const { languages, language, originalPath, t } = useI18next();
+  const languageSwitchLabel = t('navigation.language-aria');
 
   return (
-    <div className={className}>
-      {languages.map((languageOfLink) => {
-        return (
-          <StyledLanguageLink
-            key={languageOfLink}
-            to={
-              language !== languageOfLink && translation
-                ? translation
-                : originalPath
-            }
-            language={languageOfLink}
-            selected={language === languageOfLink}
-          >
-            {languageOfLink}
-          </StyledLanguageLink>
-        );
-      })}
-    </div>
+    <nav aria-label={languageSwitchLabel} className={className}>
+      <ul>
+        {languages.map((languageOfLink) => {
+          return (
+            <UnstyledListItem key={languageOfLink}>
+              <StyledLanguageLink
+                to={
+                  language !== languageOfLink && translation
+                    ? translation
+                    : originalPath
+                }
+                language={languageOfLink}
+                selected={language === languageOfLink}
+              >
+                {languageOfLink}
+              </StyledLanguageLink>
+            </UnstyledListItem>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
