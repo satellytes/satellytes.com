abbec4b6d24eaf55c3706a2ecfcdabd828ca3c67
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2021-09-27 08:43:38 +0200

    chore: align rss blog feed title with company name

--- src/components/language-switch/language-switch.tsx
+++ src/components/language-switch/language-switch.tsx
@@ -3,11 +3,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from '../links/links';
 
-const UnstyledListItem = styled.li`
-  list-style-type: none;
-  display: inline;
-`;
-
 const StyledLanguageLink = styled(Link)<{ selected?: boolean }>`
   margin: 1px 0 1px 12px;
   font-weight: bold;
@@ -28,25 +23,22 @@ export const LanguageSwitch = ({
 
   return (
     <nav aria-label={languageSwitchLabel} className={className}>
-      <ul>
-        {languages.map((languageOfLink) => {
-          return (
-            <UnstyledListItem key={languageOfLink}>
-              <StyledLanguageLink
-                to={
-                  language !== languageOfLink && translation
-                    ? translation
-                    : originalPath
-                }
-                language={languageOfLink}
-                selected={language === languageOfLink}
-              >
-                {languageOfLink}
-              </StyledLanguageLink>
-            </UnstyledListItem>
-          );
-        })}
-      </ul>
+      {languages.map((languageOfLink) => {
+        return (
+          <StyledLanguageLink
+            key={languageOfLink}
+            to={
+              language !== languageOfLink && translation
+                ? translation
+                : originalPath
+            }
+            language={languageOfLink}
+            selected={language === languageOfLink}
+          >
+            {languageOfLink}
+          </StyledLanguageLink>
+        );
+      })}
     </nav>
   );
 };
