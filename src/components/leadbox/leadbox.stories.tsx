ff11d31e0b8041838ebb50851cf519b3e4a5d447
Felix Hofmann <57712895+hofmann-felix@users.noreply.github.com>
2021-11-09 07:54:28 +0100

    feat: add item switch to storybook
    Co-authored-by: Felix Hamann <fischeversenker@users.noreply.github.com>

--- src/components/leadbox/leadbox.stories.tsx
+++ src/components/leadbox/leadbox.stories.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Leadbox } from './leadbox';
 import { Astronaut } from '../icons/illustrations/astronaut';
+import { SpaceShuttle } from '../icons/illustrations/space-shuttle';
+
+const icons = { Astronaut: (<Astronaut />), SpaceShuttle: (<SpaceShuttle />) };
 
 export default {
   component: Leadbox,
@@ -9,6 +12,12 @@ export default {
   parameters: {
     layout: 'fullscreen',
   },
+  argTypes: {
+    icon: {
+      options: Object.keys(icons),
+      mapping: icons,
+    }
+  }
 } as ComponentMeta<typeof Leadbox>;
 
 const Template: ComponentStory<typeof Leadbox> = (args) => (
