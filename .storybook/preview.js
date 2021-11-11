e11526cc94d8565bcf3d6b9d4275612ac21cb049
Felix Hofmann <57712895+hofmann-felix@users.noreply.github.com>
2021-11-11 20:37:19 +0100

    fix: add lightTheme to storybook
    Co-authored-by: Felix Hamann <fischeversenker@users.noreply.github.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,7 +7,7 @@ import { i18n } from './i18next';
 addDecorator((storyFn) => (
   <>
     <ThemeProvider theme={theme}>
-      <GlobalStyle $lightTheme />
+      <GlobalStyle $lightTheme={true} />
       {storyFn()}
     </ThemeProvider>
   </>
