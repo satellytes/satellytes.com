f6789811f1cb79a90c4aacec1df494190f719817
Jonathan Ostertag <jonathan.ostertag@satellytes.com>
2021-11-12 12:50:21 +0100

    feat: add onChange property for access to checked still some design mistakes

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -2,11 +2,16 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Checkbox = (props) => {
-  const [BoxChecked, setBoxChecked] = useState(true);
+  const [checked, setChecked] = useState(true);
+
+  const handleChange = () => {
+    setChecked(!checked);
+    props.onChange(checked);
+  };
 
   const HiddenCheckbox = styled.input.attrs({
     type: 'checkbox',
-    onChange: () => setBoxChecked(!BoxChecked),
+    onChange: handleChange,
   })`
     height: 1px;
     width: 1px;
@@ -16,39 +21,55 @@ const Checkbox = (props) => {
   `;
 
   const Icon = styled.svg`
+    display: inline-block;
     fill: none;
-    stroke: white;
-    stroke-width: 2px;
+    padding: 9.6px;
   `;
 
   const StyledCheckbox = styled.div`
     display: inline-block;
-    width: 16px;
-    height: 16px;
+    width: 24px;
+    height: 24px;
+
+    background: #f7f8fa;
+    border-radius: 4px;
 
     ${Icon} {
-      visibility: ${() => (BoxChecked ? 'visible' : 'hidden')};
+      visibility: ${checked ? 'visible' : 'hidden'};
     }
   `;
 
   const CheckboxContainer = styled.div`
     display: inline-block;
-    vertical-align: middle;
+    padding: 24px;
+  `;
+
+  const CheckboxLabel = styled.label`
+    margin-left: 12px;
   `;
 
   return (
-    <label>
-      <CheckboxContainer>
+    <CheckboxContainer>
+      <CheckboxLabel>
         <HiddenCheckbox />
         <StyledCheckbox>
-          <Icon viewBox="0 0 24 24">
-            <polyline points="20 6 9 17 4 12" />
+          <Icon
+            width="12"
+            height="12"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.2002 7.59995L4.15404 10.7999L10.8002 1.19995"
+              stroke="#202840"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            />
           </Icon>
         </StyledCheckbox>
-      </CheckboxContainer>
-      <span>{props.label}</span>
-      <span>{BoxChecked ? 'Checked' : 'Unchecked'}</span>
-    </label>
+        <span>{props.label}</span>
+      </CheckboxLabel>
+    </CheckboxContainer>
   );
 };
 
