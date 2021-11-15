67bb268a0ac656dea9ef2e0cabe9f7bb80d78068
Georgios Kaleadis <georgios.kaleadis@satellytes.com>
2021-11-15 11:15:33 +0100

    chore: add story for the navigation component (#180)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import { addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../src/components/layout/theme';
 import { GlobalStyle } from '../src/components/layout/global-style';
+import { i18n } from './i18next';
 
 addDecorator((storyFn) => (
   <>
@@ -19,5 +20,11 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  i18n,
+  locale: 'en',
+  locales: {
+    en: 'English',
+    de: 'Deutsch',
   },
 };
