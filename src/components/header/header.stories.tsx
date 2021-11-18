bed028130824e293b60e135d0159ba400473ba8e
Felix Hofmann <felix.hofmann@satellytes.com>
2021-11-18 10:06:05 +0100

    feat: add stories for different styles

--- src/components/header/header.stories.tsx
+++ src/components/header/header.stories.tsx
@@ -15,10 +15,19 @@ const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
 export const Regular = Template.bind({});
 Regular.args = {
   siteTitle: 'Satellytes',
+  $lightTheme: false,
+  showLanguageSwitch: true,
 };
 
-export const WithLanguageSwitch = Template.bind({});
-WithLanguageSwitch.args = {
+export const WithLightTheme = Template.bind({});
+WithLightTheme.args = {
   ...Regular.args,
-  showLanguageSwitch: true,
+  $lightTheme: true,
+};
+
+export const WithLightThemeTransparent = Template.bind({});
+WithLightThemeTransparent.args = {
+  ...Regular.args,
+  $lightTheme: true,
+  transparent: true,
 };
