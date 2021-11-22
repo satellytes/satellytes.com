a1fdf27ef4016cff44826f839529890be6c86f48
Jonathan Ostertag <jonathan.ostertag@satellytes.com>
2021-11-22 18:11:52 +0100

    chore: move declaration of Text Styles into export

--- src/components/typography/typography-v2.ts
+++ src/components/typography/typography-v2.ts
@@ -1,185 +1,150 @@
-import React from 'react';
 import { css } from 'styled-components';
 
-const timestamp = css`
-  font-weight: normal;
-  font-size: 14px;
-  line-height: 110%;
-  letter-spacing: -0.01em;
-
-  color: #000000;
-`;
-
-const toplineR = css`
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 110%;
-
-  color: #000000;
-`;
-
-const toplineS = css`
-  font-weight: bold;
-  font-size: 14px;
-  line-height: 110%;
-
-  color: #000000;
-`;
-
-const headlineXL = css`
-  font-weight: bold;
-  font-size: 48px;
-  line-height: 110%;
-
-  letter-spacing: -0.01em;
-
-  color: #000000;
-`;
-
-const headlineL = css`
-  font-weight: bold;
-  font-size: 32px;
-  line-height: 110%;
-
-  letter-spacing: -0.01em;
-
-  color: #000000;
-`;
-
-const headlineM = css`
-  font-weight: bold;
-  font-size: 28px;
-  line-height: 110%;
-
-  letter-spacing: -0.01em;
-
-  color: #000000;
-`;
-
-const headlineS = css`
-  font-weight: bold;
-  font-size: 20px;
-  line-height: 110%;
-
-  letter-spacing: -0.01em;
-
-  color: #000000;
-`;
-
-const headlineXS = css`
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 150%;
-
-  letter-spacing: -0.01em;
-
-  color: #000000;
-`;
-
-const textL = css`
-  font-weight: normal;
-  font-size: 20px;
-  line-height: 150%;
-
-  letter-spacing: -0.01em;
-
-  color: #000000;
-`;
-
-const textR = css`
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 150%;
-
-  letter-spacing: -0.01em;
-
-  color: #000000;
-`;
-
-const textS = css`
-  font-weight: normal;
-  font-size: 14px;
-  line-height: 150%;
-
-  letter-spacing: -0.01em;
-
-  color: #000000;
-`;
-
-const textXS = css`
-  font-weight: normal;
-  font-size: 12px;
-  line-height: 150%;
-
-  letter-spacing: -0.01em;
-
-  color: #000000;
-`;
-
-const quote = css`
-  font-style: italic;
-  font-weight: normal;
-  font-size: 20px;
-  line-height: 150%;
-
-  color: #000000;
-`;
-
-const code = css`
-  font-family: Monaco;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 150%;
-
-  color: #000000;
-`;
-
-const menuItemR = css`
-  font-weight: bold;
-  font-size: 48px;
-  line-height: 150%;
-
-  letter-spacing: -0.01em;
-
-  color: #000000;
-`;
-
-const menuItemS = css`
-  font-weight: bold;
-  font-size: 32px;
-  line-height: 150%;
-
-  letter-spacing: -0.01em;
-
-  color: #000000;
-`;
-
-// MenuLaguage has the exact same configuration as MenuMeta
-const menuMeta = css`
-  font-weight: bold;
-  font-size: 14px;
-  line-height: 110%;
-
-  color: #000000;
-`;
-
 export const TextStyles = {
-  timestamp,
-  toplineR,
-  toplineS,
-  headlineXL,
-  headlineL,
-  headlineM,
-  headlineS,
-  headlineXS,
-  textL,
-  textR,
-  textS,
-  textXS,
-  quote,
-  code,
-  menuItemR,
-  menuItemS,
-  menuMeta,
+  timestamp: css`
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 110%;
+    letter-spacing: -0.01em;
+
+    color: #000000;
+  `,
+  toplineR: css`
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 110%;
+
+    color: #000000;
+  `,
+  toplineS: css`
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 110%;
+
+    color: #000000;
+  `,
+  headlineXL: css`
+    font-weight: bold;
+    font-size: 48px;
+    line-height: 110%;
+
+    letter-spacing: -0.01em;
+
+    color: #000000;
+  `,
+  headlineL: css`
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 110%;
+
+    letter-spacing: -0.01em;
+
+    color: #000000;
+  `,
+  headlineM: css`
+    font-weight: bold;
+    font-size: 28px;
+    line-height: 110%;
+
+    letter-spacing: -0.01em;
+
+    color: #000000;
+  `,
+  headlineS: css`
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 110%;
+
+    letter-spacing: -0.01em;
+
+    color: #000000;
+  `,
+  headlineXS: css`
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 150%;
+
+    letter-spacing: -0.01em;
+
+    color: #000000;
+  `,
+  textL: css`
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 150%;
+
+    letter-spacing: -0.01em;
+
+    color: #000000;
+  `,
+  textR: css`
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 150%;
+
+    letter-spacing: -0.01em;
+
+    color: #000000;
+  `,
+  textS: css`
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 150%;
+
+    letter-spacing: -0.01em;
+
+    color: #000000;
+  `,
+  textXS: css`
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 150%;
+
+    letter-spacing: -0.01em;
+
+    color: #000000;
+  `,
+  quote: css`
+    font-style: italic;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 150%;
+
+    color: #000000;
+  `,
+  code: css`
+    font-family: Monaco;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 150%;
+
+    color: #000000;
+  `,
+  menuItemR: css`
+    font-weight: bold;
+    font-size: 48px;
+    line-height: 150%;
+
+    letter-spacing: -0.01em;
+
+    color: #000000;
+  `,
+  menuItemS: css`
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 150%;
+
+    letter-spacing: -0.01em;
+
+    color: #000000;
+  `,
+  // MenuLaguage has the exact same configuration as MenuMeta
+  menuMeta: css`
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 110%;
+
+    color: #000000;
+  `,
 };
