777b501eb9ea40c820aeab3d78d640cc2b3a0bfe
Jonathan Ostertag <jonathan.ostertag@satellytes.com>
2021-11-22 17:03:03 +0100

    chore: rename RegularButton files into Button

--- src/components/buttons/button.stories.tsx
+++ src/components/buttons/button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import Button from './regular-button';
+import Button from './button';
 
 export default {
   component: Button,
