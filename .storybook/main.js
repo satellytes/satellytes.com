9c7f250ea4b63bf910c5fc5058546fec6ff96143
Felix Hofmann <felix.hofmann@satellytes.com>
2021-11-22 14:09:27 +0100

    feat: add mdx support for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     'storybook-addon-gatsby',
     'storybook-react-i18next',
+    '@storybook/addon-docs',
   ],
   core: {
     builder: 'webpack5',
