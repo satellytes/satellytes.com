83c192e05cc44c21dee5eab279db062dbc35c4c6
Jonathan Ostertag <jonathan.ostertag@satellytes.com>
2021-11-26 12:54:39 +0100

    feat: add ImageBlockContent

--- src/components/image-contentBlock/image-contentBlock.stories.tsx
+++ src/components/image-contentBlock/image-contentBlock.stories.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import ExampleImage from '../../images/office/sy-office-01.jpg';
 import ImageContentBlock from './image-contentBlock';
+import styled from 'styled-components';
 
 export default {
   component: ImageContentBlock,
   title: 'Components/ImageContentBlock',
 } as ComponentMeta<typeof ImageContentBlock>;
 
+const SampleImage = styled.img`
+  width: 100%;
+`;
+
 const Template: ComponentStory<typeof ImageContentBlock> = (args) => (
-  <ImageContentBlock {...args} />
+  <ImageContentBlock {...args}>
+    <SampleImage alt="" src={ExampleImage} />
+  </ImageContentBlock>
 );
 
 export const Regular = Template.bind({});
-Regular.args = {};
+Regular.args = {
+  textAlign: 'right',
+  description:
+    'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. ',
+  attribution: {
+    source: 'www.somelink.de',
+    creator: 'Banksy',
+  },
+};
--- src/components/image-contentBlock/image-contentBlock.tsx
+++ src/components/image-contentBlock/image-contentBlock.tsx
@@ -1,15 +1,115 @@
 import React from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
 import styled from 'styled-components';
+import { down } from '../breakpoint/breakpoint';
+import { theme } from '../layout/theme';
+import { TextStyles } from '../typography/typography-v2';
 
-const ImageWrapper = styled.div``;
+const BlockWrapper = styled.div<{
+  textAlign: 'right' | 'left' | 'bottom';
+}>`
+  display: inline-flex;
+  flex-direction: ${({ textAlign }) =>
+    textAlign === 'right'
+      ? 'row'
+      : textAlign === 'left'
+      ? 'row-reverse'
+      : 'column'};
 
-const ImageContentBlock = (props) => {
+  width: 100%;
+
+  ${down('sm')} {
+    flex-direction: column;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  display: inline;
+  position relative;
+`;
+
+const AttributionContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.2);
+  display: block;
+
+  padding: 6px 8px;
+  margin-bottom: 5px;
+
+  font-size: 10px;
+  line-height: 110%;
+  color: #ffffff;
+`;
+
+const StyledLink = styled.a`
+  color: #ffffff;
+`;
+
+const TextWrapper = styled.div<{
+  textAlign: 'right' | 'left' | 'bottom';
+}>`
+  ${({ textAlign }) =>
+    textAlign === 'right'
+      ? 'margin-left: 24px;'
+      : textAlign === 'left'
+      ? 'margin-right: 24px;'
+      : 'margin-top: 12px;'}
+
+  ${({ textAlign }) =>
+    textAlign === 'bottom' ? TextStyles.textXS : TextStyles.textS}
+
+  ${({ textAlign }) =>
+    textAlign === 'bottom' ? 'width 0; min-width:100%;' : ''}
+  ${({ textAlign }) => (textAlign !== 'bottom' ? 'max-width: 228px;' : '')}
+
+  ${down('sm')} {
+    ${TextStyles.textXS}
+
+    margin: 12px 0;
+    width 0;
+    min-width: 100%;
+    max-width: 100;
+  }
+
+  ${theme.palette.text.default};
+`;
+
+interface ImageContentBlockProps {
+  children: React.ReactNode;
+  textAlign: 'right' | 'left' | 'bottom';
+  description: string;
+  attribution: {
+    source: string;
+    creator: string;
+  };
+}
+
+const ImageContentBlock = ({
+  children,
+  textAlign,
+  description,
+  attribution,
+}: ImageContentBlockProps) => {
   return (
-    <ImageWrapper>
-      <StaticImage alt="Image" src="../../images/office/sy-office-01.jpg" />
-      Hallo
-    </ImageWrapper>
+    <BlockWrapper textAlign={textAlign}>
+      <ImageWrapper>
+        {children}
+        {attribution && (
+          <AttributionContainer>
+            Photo by{' '}
+            <StyledLink
+              rel="nofollow noreferrer"
+              target="_blank"
+              href={attribution.source}
+            >
+              {attribution.creator}
+            </StyledLink>
+          </AttributionContainer>
+        )}
+      </ImageWrapper>
+      <TextWrapper textAlign={textAlign}>{description}</TextWrapper>
+    </BlockWrapper>
   );
 };
 
