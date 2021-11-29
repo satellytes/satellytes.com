063f072d7be4f200c19898c736b9685695769ed3
Georgios Kaleadis (Satellytes) <georgios.kaleadis@satellytes.com>
2021-11-29 11:26:57 +0100

    fix(build): force parity of building with SVGR in storybook and gatsby

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -4,9 +4,23 @@ declare module '*.eot';
 declare module '*.ttf';
 declare module '*.png';
 
-type SvgrComponent = React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+/**
+ * This type matches the svg export as configured in storybook for SVGR.
+ *
+ * It's actually different to the linked reference and hwo Gatsby
+ * does the processing. In Gatsby there are two loaders registered for svg files
+ * that's why we get the file path form the file loader for the default export
+ * and SVGR then switches to the named export `ReactComponent`.
+ *
+ * In Storybook we don't need any file loader so we force SVGR to
+ * export into the named export `ReactComponent` to force a parity between
+ * the two build systems involved.
+ *
+ * Reference:
+ * https://github.com/gregberge/svgr/issues/38#issuecomment-717602727
+ */
 
 declare module '*.svg' {
-  const svg: string;
-  export default svg;
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export { ReactComponent };
 }
