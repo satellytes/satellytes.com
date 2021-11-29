9dad1b8c659dbb37ae7aaa82927a972bd2672983
Jonathan Ostertag <jonathan.ostertag@satellytes.com>
2021-11-29 14:05:42 +0100

    chore: kebap-case

--- src/components/image-content-block/image-content-block.stories.tsx
+++ src/components/image-content-block/image-content-block.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import ExampleImage from '../../images/office/sy-office-01.jpg';
-import ImageContentBlock from './image-contentBlock';
+import ImageContentBlock from './image-content-block';
 import styled from 'styled-components';
 
 export default {
