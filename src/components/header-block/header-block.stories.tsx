a5c07cb177164973fe665527cbb5d234b866a28f
Jonathan Ostertag <jonathan.ostertag@satellytes.com>
2021-11-29 17:06:57 +0100

    chore: remove unused import

--- src/components/header-block/header-block.stories.tsx
+++ src/components/header-block/header-block.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentStory } from '@storybook/react';
 import { HeaderBlock } from './header-block';
 
 const Template: ComponentStory<typeof HeaderBlock> = (args) => (
