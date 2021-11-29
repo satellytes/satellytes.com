07e2040d4e548c0a6d4c368b1369b5a8acd9ab95
Jonathan Ostertag <jonathan.ostertag@satellytes.com>
2021-11-29 17:15:14 +0100

    chore: enhance HeaderBlock documentation (#223)
    * chore: enhance HeaderBlock documentation
    
    * fix: remove default export
    -> Documentation visible in netlify storybook
    
    * fix: make .mdx file to come to an effect
    
    * chore: remove unused import

--- src/components/header-block/header-block.stories.tsx
+++ src/components/header-block/header-block.stories.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import HeaderBlock from './header-block';
-
-export default {
-  component: HeaderBlock,
-  title: 'Components/HeaderBlock',
-} as ComponentMeta<typeof HeaderBlock>;
+import { ComponentStory } from '@storybook/react';
+import { HeaderBlock } from './header-block';
 
 const Template: ComponentStory<typeof HeaderBlock> = (args) => (
-  <HeaderBlock {...args}>
-    Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Morbi
-    leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam
-    eget risus varius blandit sit amet non magna.
-  </HeaderBlock>
+  <HeaderBlock {...args}></HeaderBlock>
 );
 
 export const Regular = Template.bind({});
@@ -23,4 +14,15 @@ Regular.args = {
     'vor 3 Tagen · von Fabian Dietenberger, Frontend Engineer, Satellytes',
   illustration: 'rocket_011',
   large: true,
+  children:
+    'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna.',
+};
+
+export const LandingPageExample = Template.bind({});
+LandingPageExample.args = {
+  topline: 'Unsere Services',
+  headline: 'Development & Design',
+  large: true,
+  children: `Wir haben großen Spaß an Technologie und freuen uns auf neue Herausforderungen. Dabei fokussieren wir uns auf langfristige Engagements im Konzerngeschäft.
+              Neuen Aufgaben begegnen wir immer mit angemessenem Respekt. Wir streben stets hochwertige und zeitgemäße Lösungen an – die Wahl der Technologie ist für uns dabei sekundär. Gern unterstützen wir auch Sie in den folgenden Disziplinen:`,
 };
