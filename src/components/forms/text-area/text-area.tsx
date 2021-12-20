a28cc6c0c97b58611e1e1580d85bf45c103e67b9
Felix Hofmann <felix.hofmann@satellytes.com>
2021-12-20 11:24:14 +0100

    feat: add TextArea component

---
+++ src/components/forms/text-area/text-area.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import { TextStyles } from '../../typography';
+import { theme } from '../../layout/theme';
+
+const StyledTextArea = styled.textarea<{ hasError?: boolean }>`
+  height: 160px;
+  width: 100%;
+  padding: 19px 16px;
+  resize: vertical;
+
+  ${TextStyles.textR}
+
+  background: ${theme.palette.background.leadbox};
+  border: 2px solid
+    ${(props) =>
+      props.hasError
+        ? theme.palette.text.errorMessage
+        : theme.palette.background.leadbox};
+`;
+
+const Label = styled.label`
+  ${TextStyles.textXS}
+  display: block;
+  margin-bottom: 4px;
+
+  color: ${theme.palette.text.default};
+`;
+
+const StyledErrorMessage = styled.span`
+  ${TextStyles.textXS}
+  font-weight: 700;
+  display: block;
+  margin-top: 4px;
+
+  color: ${theme.palette.text.errorMessage};
+`;
+
+export interface TextAreaProps {
+  label?: string;
+  name: string;
+  errorMessage: string;
+  inputRef: any;
+  required?: boolean;
+}
+
+export const TextArea = ({
+  label,
+  name,
+  errorMessage,
+  inputRef,
+  required,
+}: TextAreaProps) => {
+  return (
+    <>
+      {label && (
+        <Label htmlFor={name}>
+          {label} {required && <span aria-hidden={true}>*</span>}
+        </Label>
+      )}
+      <StyledTextArea
+        {...inputRef}
+        hasError={errorMessage}
+        id={name}
+        aria-required={required}
+      />
+      {errorMessage && <StyledErrorMessage>{errorMessage}</StyledErrorMessage>}
+    </>
+  );
+};
