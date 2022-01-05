88b9a96f153fa772cd8bc1697ebe5e47ae3ac2de
Georgios Kaleadis (Satellytes) <georgios.kaleadis@satellytes.com>
2022-01-05 12:54:46 +0100

    chore: test

--- src/api/contact-form.ts
+++ src/api/contact-form.ts
@@ -1,4 +1,24 @@
-export default function handler(req, res) {
+import { WebClient, LogLevel } from '@slack/web-api';
+
+export default async function handler(req, res) {
+  const channelId = 'website';
+
+  const client = new WebClient(process.env.SLACK_BOT_SY_TOKEN, {
+    logLevel: LogLevel.DEBUG,
+  });
+
+  try {
+    // Call the chat.postMessage method using the WebClient
+    const result = await client.chat.postMessage({
+      channel: channelId,
+      text: 'Hello world',
+    });
+
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+
   console.log(`submitted form`, req.body);
   res.status(200).json({ hello: `world` });
 }
