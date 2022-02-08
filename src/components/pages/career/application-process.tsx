819bee2622d02a70cbaeb2b4a98e34427f9f227c
Felix Hofmann <felix.hofmann@satellytes.com>
2022-02-08 14:21:36 +0100

    fix: stretch accordion to 100% width on mobile

--- src/components/pages/career/application-process.tsx
+++ src/components/pages/career/application-process.tsx
@@ -3,9 +3,18 @@ import { Accordion, AccordionSection } from '../../ui/accordion/accordion';
 import styled from 'styled-components';
 import { SectionHeader } from '../../content/section-header/section-header';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
+import { up } from '../../support/breakpoint';
 
 const Spacer = styled.div`
   margin-bottom: 48px;
+`;
+
+const AccordionWrapper = styled.div`
+  margin: 0 -24px;
+
+  ${up('sm')} {
+    margin: 0;
+  }
 `;
 
 export const ApplicationProcess = () => {
@@ -18,33 +27,35 @@ export const ApplicationProcess = () => {
 
       <Spacer />
 
-      <Accordion defaultIndex={0}>
-        <AccordionSection
-          title={t('career.application-process.accordion.0.title')}
-          illustration={'scientistB_007'}
-        >
-          {t('career.application-process.accordion.0.paragraph')}
-        </AccordionSection>
+      <AccordionWrapper>
+        <Accordion defaultIndex={0}>
+          <AccordionSection
+            title={t('career.application-process.accordion.0.title')}
+            illustration={'scientistB_007'}
+          >
+            {t('career.application-process.accordion.0.paragraph')}
+          </AccordionSection>
 
-        <AccordionSection
-          title={t('career.application-process.accordion.1.title')}
-          illustration={'astronaut_015'}
-        >
-          {t('career.application-process.accordion.1.paragraph')}
-        </AccordionSection>
+          <AccordionSection
+            title={t('career.application-process.accordion.1.title')}
+            illustration={'astronaut_015'}
+          >
+            {t('career.application-process.accordion.1.paragraph')}
+          </AccordionSection>
 
-        <AccordionSection
-          title={t('career.application-process.accordion.2.title')}
-        >
-          {t('career.application-process.accordion.2.paragraph')}
-        </AccordionSection>
+          <AccordionSection
+            title={t('career.application-process.accordion.2.title')}
+          >
+            {t('career.application-process.accordion.2.paragraph')}
+          </AccordionSection>
 
-        <AccordionSection
-          title={t('career.application-process.accordion.3.title')}
-        >
-          {t('career.application-process.accordion.3.paragraph')}
-        </AccordionSection>
-      </Accordion>
+          <AccordionSection
+            title={t('career.application-process.accordion.3.title')}
+          >
+            {t('career.application-process.accordion.3.paragraph')}
+          </AccordionSection>
+        </Accordion>
+      </AccordionWrapper>
     </>
   );
 };
