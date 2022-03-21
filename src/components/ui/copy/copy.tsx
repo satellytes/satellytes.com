6d3a7c9bc4e59382e1d5d04b7511aae9f176c877
Jonathan Ostertag <jonathan.ostertag@satellytes.com>
2022-03-21 13:08:16 +0100

    feat: add text

--- src/components/ui/copy/copy.tsx
+++ src/components/ui/copy/copy.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import { Trans, useTranslation } from 'react-i18next';
+import styled, { css } from 'styled-components';
 import { IconButton } from '../buttons/icon-button';
 import { Icon } from '../icon/icon';
 
@@ -28,17 +29,26 @@ const CheckmarkContainer = styled.span`
   padding: 0;
 `;
 
-const CopyWrapper = styled.div`
+const CopyWrapper = styled.div<{ clicked: boolean }>`
   display: inline-flex;
   justify-content: center;
   align-items: center;
+  gap: 8px;
   height: 32px;
-  width: 32px;
   padding: 0;
+
+  ${(props) =>
+    props.clicked &&
+    css`
+      padding: 8px;
+      border-radius: 3px;
+      background-color: white;
+    `}
 `;
 
 export const Copy = ({ clickedDuration, text, className }: CopyProps) => {
   const [clicked, setClicked] = useState(false);
+  const { t } = useTranslation();
 
   const handleClick = () => {
     setClicked(true);
@@ -52,14 +62,19 @@ export const Copy = ({ clickedDuration, text, className }: CopyProps) => {
   };
 
   return (
-    <CopyWrapper id="copy-container" className={className}>
-      {clicked ? (
-        <CheckmarkContainer>
-          <Icon show="checkmark_bold" />
-        </CheckmarkContainer>
-      ) : (
-        <IconButton icon={'clone'} onClick={handleClick}></IconButton>
-      )}
-    </CopyWrapper>
+    <div className={className}>
+      <CopyWrapper id="copy-container" clicked={clicked}>
+        {clicked ? (
+          <>
+            <span>{t('blog.copied')}</span>
+            <CheckmarkContainer>
+              <Icon show="checkmark_bold" />
+            </CheckmarkContainer>
+          </>
+        ) : (
+          <IconButton icon={'clone'} onClick={handleClick}></IconButton>
+        )}
+      </CopyWrapper>
+    </div>
   );
 };
