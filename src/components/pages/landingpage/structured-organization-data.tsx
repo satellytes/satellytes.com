e8c51c67cd0dbc7011f8834f2c654e14e0ba17d8
Jonathan Ostertag <jonathan.ostertag@satellytes.com>
2022-03-29 15:30:33 +0200

    chore: add comment

--- src/components/pages/landingpage/structured-organization-data.tsx
+++ src/components/pages/landingpage/structured-organization-data.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+/**
+ * This adds Structured Data for the Satellytes Organization based on https://schema.org/Organization
+ * to provide Information for the logo and the Google knowledge panel in general
+ *
+ * Read https://developers.google.com/search/docs/advanced/structured-data/logo for more information about the logo
+ */
 export const StructuredOrganizationData = () => {
   const data = {
     '@context': 'https://schema.org',
