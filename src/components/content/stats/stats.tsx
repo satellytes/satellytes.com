e93deb65a3f0da6599670540361b6d0d7df6b984
Jonathan Ostertag <jonathan.ostertag@satellytes.com>
2022-04-26 12:16:58 +0200

    chore: remove unused

--- src/components/content/stats/stats.tsx
+++ src/components/content/stats/stats.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { down, up } from '../../support/breakpoint';
+import { up } from '../../support/breakpoint';
 import { TextStyles } from '../../typography';
 
 interface StatProps {
