789d08cdd47d08455ffd9093bab1b0a66867417b
timon-so <timon.sommer@satellytes.com>
2022-09-09 13:30:11 +0200

    chore: removed unused dependencies and imports

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -2,7 +2,6 @@ import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer';
 import type { GatsbyNode } from 'gatsby';
 import { createFilePath } from 'gatsby-source-filesystem';
-import { inspect } from 'util';
 import { createPreviewCards } from './gatsby/create-node/create-preview-cards';
 import { createBlogPostOverviewPages } from './gatsby/create-pages/create-blog-post-overview-pages';
 import { createBlogPosts } from './gatsby/create-pages/create-blog-posts';
