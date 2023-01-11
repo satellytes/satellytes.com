cc2ed44908827f3ced46524e52201e948521f20a
Elisa Alcocer <elisa.alcocer@satellytes.com>
2023-01-11 17:28:43 +0100

    chore: change carrer ordering and add images spacer

--- src/components/pages/career/career-page.tsx
+++ src/components/pages/career/career-page.tsx
@@ -11,13 +11,28 @@ import { useTranslation } from 'gatsby-plugin-react-i18next';
 import { SyPersonioJob } from '../../../types';
 import { ImageHero } from '../../content/heroes';
 import { IGatsbyImageData } from 'gatsby-plugin-image';
+import { ImageSpacer } from '../../pages/landingpage/image-spacer';
 
 interface CareerPageProps {
   positions: SyPersonioJob[];
   heroImageData: IGatsbyImageData;
+  officeImages: OfficeImages;
 }
 
-export const CareerPage = ({ positions, heroImageData }: CareerPageProps) => {
+interface OfficeImage {
+  relativePath: string;
+  childImageSharp: {
+    gatsbyImageData: IGatsbyImageData;
+  };
+}
+
+type OfficeImages = { [relativePath: string]: OfficeImage };
+
+export const CareerPage = ({
+  positions,
+  heroImageData,
+  officeImages,
+}: CareerPageProps) => {
   const { t } = useTranslation();
 
   const leadbox: LeadboxProps = {
@@ -48,16 +63,20 @@ export const CareerPage = ({ positions, heroImageData }: CareerPageProps) => {
       </ContentBlockContainer>
 
       <ContentBlockContainer>
-        <Openings jobs={positions} />
-      </ContentBlockContainer>
-
-      <ContentBlockContainer>
         <ApplicationProcess />
       </ContentBlockContainer>
 
       <ContentBlockContainer>
+        <Openings jobs={positions} />
+      </ContentBlockContainer>
+
+      <ImageSpacer image={officeImages['office/sy-office-03.jpg']} />
+
+      <ContentBlockContainer>
         <Culture />
       </ContentBlockContainer>
+
+      <ImageSpacer image={officeImages['office/sy-office-02.jpg']} />
 
       <ContentBlockContainer>
         <Perks />
--- src/pages/career.tsx
+++ src/pages/career.tsx
@@ -6,15 +6,30 @@ import { CareerPage } from '../components/pages/career/career-page';
 import { SyPersonioJob } from '../types';
 import { IGatsbyImageData } from 'gatsby-plugin-image';
 
+interface OfficeImage {
+  relativePath: string;
+  childImageSharp: {
+    gatsbyImageData: IGatsbyImageData;
+  };
+}
+
 interface CareerPageQueryProps {
   allSyPersonioJob: {
     nodes: SyPersonioJob[];
+  };
+  officeImages: {
+    nodes: OfficeImage[];
   };
   hero: IGatsbyImageData;
 }
 
 const Career = (props: PageProps<CareerPageQueryProps>) => {
   const { t } = useTranslation();
+
+  const officeImages = props.data.officeImages.nodes.reduce((memo, image) => {
+    memo[image.relativePath] = image;
+    return memo;
+  }, {});
 
   return (
     <>
@@ -26,6 +41,7 @@ const Career = (props: PageProps<CareerPageQueryProps>) => {
       <CareerPage
         heroImageData={props.data.hero}
         positions={props.data.allSyPersonioJob.nodes}
+        officeImages={officeImages}
       />
     </>
   );
@@ -35,6 +51,16 @@ export default Career;
 
 export const CareerPageQuery = graphql`
   query ($language: String!) {
+    officeImages: allFile(filter: { relativeDirectory: { eq: "office" } }) {
+      nodes {
+        id
+        relativePath
+        childImageSharp {
+          gatsbyImageData(layout: FULL_WIDTH)
+        }
+      }
+    }
+
     hero: file(relativePath: { eq: "office/sy-office-01.jpg" }) {
       childImageSharp {
         gatsbyImageData(layout: FULL_WIDTH)
