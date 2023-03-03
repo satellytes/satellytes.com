5437054d73e149d66521a798c0bf455a3b770303
Felix Hofmann <57712895+hofmann-felix@users.noreply.github.com>
2023-03-03 14:30:25 +0100

    feat(CareerPage): new teaser grid design (#701)
    * feat(CareerPage): new teaser grid design
    
    * feat(CareerPage): add teaser grid to openings
    
    * feat: use grid auto flow for mobile layout

--- src/components/pages/career/openings.tsx
+++ src/components/pages/career/openings.tsx
@@ -4,16 +4,24 @@ import styled from 'styled-components';
 import { TextStyles } from '../../typography';
 import { SyPersonioJob } from '../../../types';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
-import { TeaserGrid } from '../../content/teaser/teaser-grid';
 import { up } from '../../support/breakpoint';
+import { CareerTeaserGrid } from './career-teaser-grid';
 
 const SectionHeadline = styled.h2`
   ${TextStyles.headlineL}
   margin: 0;
-  margin-bottom: 80px;
+  margin-bottom: 48px;
 
   ${up('md')} {
     ${TextStyles.headlineXL}
+  }
+`;
+
+const OpeningsTeaseGrid = styled(CareerTeaserGrid)`
+  margin-top: 48px;
+
+  ${up('md')} {
+    margin-top: 60px;
   }
 `;
 
@@ -23,16 +31,17 @@ interface OpeningsProps {
 
 export const Openings = (props: OpeningsProps) => {
   const { t } = useTranslation();
+
   return (
     <div>
       <SectionHeadline>{t('career.openings.headline')}</SectionHeadline>
-      <TeaserGrid>
+      <OpeningsTeaseGrid>
         {props.jobs.map((item) => (
           <Teaser title={item.name} linkTo={item.slug} key={item.id}>
             {item.short}
           </Teaser>
         ))}
-      </TeaserGrid>
+      </OpeningsTeaseGrid>
     </div>
   );
 };
