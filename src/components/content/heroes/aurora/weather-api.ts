f09a53bddc07519a71153228ad6dbea45ce36904
noasanto <n@bensanto.com>
2023-07-11 17:58:54 +0200

    fix: weather-api.ts now return always a vaild weather.

--- src/components/content/heroes/aurora/weather-api.ts
+++ src/components/content/heroes/aurora/weather-api.ts
@@ -10,9 +10,45 @@ export async function getWeather() {
 
   try {
     const response = await axios.get(apiUrl);
-    return response.data.current.condition.text;
+    const conditionCode = response.data.current.condition.code;
+    return getWeatherDescription(conditionCode);
   } catch (error) {
     console.error('Error fetching weather:', error);
     return 'NotSet';
+  }
+}
+
+function getWeatherDescription(conditionCode) {
+  switch (conditionCode) {
+    case 1000:
+      return 'Sunny';
+    case 1003:
+    case 1006:
+      return 'Cloudy';
+    case 1063:
+    case 1180:
+    case 1183:
+    case 1186:
+    case 1189:
+    case 1192:
+    case 1195:
+    case 1198:
+    case 1201:
+    case 1204:
+    case 1207:
+    case 1240:
+    case 1243:
+      return 'Rainy';
+    case 1114:
+    case 1117:
+    case 1210:
+    case 1213:
+    case 1216:
+    case 1219:
+    case 1222:
+    case 1225:
+      return 'Snowy';
+    default:
+      return 'NotSet';
   }
 }
