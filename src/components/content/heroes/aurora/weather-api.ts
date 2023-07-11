cd07d0d2e74ff29f95b29a95d8cb412c3421456f
noasanto <n@bensanto.com>
2023-07-11 17:28:11 +0200

    fix: readded normal api

--- src/components/content/heroes/aurora/weather-api.ts
+++ src/components/content/heroes/aurora/weather-api.ts
@@ -2,12 +2,11 @@ import axios from 'axios';
 
 const API_KEY = process.env.WEATHER_API_KEY;
 const BASE_URL = 'https://api.weatherapi.com/v1/current.json';
-const BASE_URL_DEBUG = 'http://0.0.0.0:8000/v1/current.json';
 const latitude = 48.1351;
 const longitude = 11.582;
 
 export async function getWeather() {
-  const apiUrl = BASE_URL_DEBUG + `?key=${API_KEY}&q=${latitude},${longitude}`;
+  const apiUrl = BASE_URL + `?key=${API_KEY}&q=${latitude},${longitude}`;
 
   try {
     const response = await axios.get(apiUrl);
