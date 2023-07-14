7ba54d18e5de1735e3e48c1c10f5824d2e7f3a9f
Felix Hofmann <felix.hofmann@satellytes.com>
2023-07-14 14:06:47 +0200

    refactor: move clouds into own component

--- src/components/content/heroes/aurora/aurora-components.tsx
+++ src/components/content/heroes/aurora/aurora-components.tsx
@@ -1,15 +1,7 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { WeatherType } from './aurora-types';
 import { Flare, FlareType } from './flare';
-import {
-  CloudBackground,
-  CloudFrame,
-  CloudGroup,
-  CloudOverlapGroup,
-  CloudOverlay,
-  CloudShape,
-  CloudWrapper,
-} from './clouds';
+import { Clouds } from './clouds';
 import PrecipitationEffect, { PrecipitationType } from './precipitation-effect';
 import React from 'react';
 
@@ -40,12 +32,12 @@ export const AuroraBackground = styled.div<AuroraBackgroundProps>`
   top: 0;
   ${(props) =>
     props.weather === WeatherType.NotSet
-      ? `
-    background-image: url(${props.source});
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center -20vw;
-  `
+      ? css`
+          background-image: url(${props.source});
+          background-repeat: no-repeat;
+          background-size: cover;
+          background-position: center -20vw;
+        `
       : ''}
 `;
 
@@ -118,74 +110,7 @@ export const flaresByWeather: { [key in WeatherType] } = {
       />
     </>
   ),
-  [WeatherType.Cloudy]: (
-    <CloudFrame>
-      <CloudWrapper>
-        <CloudGroup>
-          <CloudOverlapGroup>
-            <CloudShape
-              height={970}
-              left={10}
-              top={50}
-              width={970}
-              background={
-                'linear-gradient(180deg,rgb(76.5, 121.12, 255) 0%,rgba(76.5, 121.12, 255, 0.15) 100%)'
-              }
-            />
-            <CloudBackground />
-            <CloudShape
-              height={1094}
-              left={35}
-              top={30}
-              width={1094}
-              background={
-                'linear-gradient(180deg, rgb(86, 80.98, 96.69) 0%, rgba(99.44, 98.73, 133.87, 0.15) 100%)'
-              }
-            />
-            <CloudOverlay height={284} width={284} top={45} left={15} />
-            <CloudOverlay
-              height={693}
-              width={693}
-              top={80}
-              left={70}
-              customStyle={'rotate(122.06deg);'}
-            />
-            <CloudOverlay height={333} width={333} top={50} left={25} />
-            <CloudOverlay height={308} width={308} top={62} left={95} />
-            <CloudOverlay height={457} width={457} top={45} left={65} />
-            <CloudOverlay
-              height={285}
-              width={285}
-              top={43}
-              left={30}
-              customStyle={`
-            background: linear-gradient(
-              180deg,
-              rgb(44, 42, 90) 0%,
-              rgba(42, 27, 113, 0.64) 100%
-            );
-            mix-blend-mode: multiply;
-            opacity: 0.1;
-          `}
-            />
-            <CloudOverlay height={150} width={150} bottom={10} right={30} />
-            <CloudOverlay height={200} width={200} top={60} left={5} />
-            <CloudOverlay height={200} width={200} top={10} left={10} />
-            <CloudOverlay height={250} width={250} top={30} left={30} />
-            <CloudOverlay height={180} width={180} top={50} left={50} />
-            <CloudOverlay height={300} width={300} top={70} left={70} />
-            <CloudOverlay height={230} width={230} top={90} left={90} />
-            <CloudOverlay height={200} width={200} top={70} left={5} />
-            <CloudOverlay height={200} width={200} top={110} left={10} />
-            <CloudOverlay height={550} width={550} top={90} left={30} />
-            <CloudOverlay height={180} width={180} top={100} left={50} />
-            <CloudOverlay height={300} width={300} top={130} left={70} />
-            <CloudOverlay height={230} width={230} top={140} left={90} />
-          </CloudOverlapGroup>
-        </CloudGroup>
-      </CloudWrapper>
-    </CloudFrame>
-  ),
+  [WeatherType.Cloudy]: <Clouds amount={20} />,
   [WeatherType.Rainy]: (
     <>
       <AuroraRainyFlareColor />
---
+++ src/components/content/heroes/aurora/clouds.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const MAX_CLOUDS_PER_GROUP = 5;
+
+interface CloudsProps {
+  amount: number;
+}
+
+const getRandomValue = (min, max) => Math.random() * (max - min) + min;
+const getRandomTransform = (distance) =>
+  `translate(${getRandomValue(-distance, distance)}px, ${getRandomValue(
+    -distance,
+    distance,
+  )}px)`;
+
+const cloudAnimation = (distance) =>
+  keyframes`
+  0% {
+    transform: ${getRandomTransform(distance)};
+  }
+  50% {
+    transform: ${getRandomTransform(distance)};
+  }
+  100% {
+    transform: ${getRandomTransform(distance)};
+  }`;
+
+const CloudsContainer = styled.div`
+  width: 100%;
+  height: 100%;
+
+  background: linear-gradient(
+    180deg,
+    rgb(74, 73, 93) 0%,
+    rgb(134, 134, 171) 100%
+  );
+`;
+
+const CloudGroupContainer = styled.div`
+  width: 25%;
+  height: 200px;
+  position: absolute;
+
+  top: calc(${() => Math.floor(Math.random() * 100)}% - 200px);
+  left: calc(${() => Math.floor(Math.random() * 100)}% - 200px);
+
+  animation: ${() => cloudAnimation(50)} ${getRandomValue(9, 12)}s alternate
+    infinite;
+`;
+
+export const Clouds = ({ amount }: CloudsProps) => {
+  return (
+    <CloudsContainer>
+      {Array.from(Array(amount).keys()).map((i) => (
+        <CloudGroup key={i} />
+      ))}
+    </CloudsContainer>
+  );
+};
+
+export const CloudGroup = () => {
+  const amount = Math.floor(Math.random() * MAX_CLOUDS_PER_GROUP) + 1;
+  console.log(amount);
+  return (
+    <CloudGroupContainer>
+      {Array.from(Array(amount).keys()).map((i) => (
+        <Cloud key={i} />
+      ))}
+    </CloudGroupContainer>
+  );
+};
+
+const Cloud = styled.div`
+  --size: ${() => Math.floor(Math.random() * 400) + 100}px;
+  width: var(--size);
+  height: var(--size);
+  background: linear-gradient(
+    180deg,
+    rgb(200, 200, 200) 0%,
+    rgba(200, 200, 200, 0.15) 100%
+  );
+  box-shadow: inset 0 0 60px #00000080;
+  opacity: ${Math.random() * 0.5 + 0.1};
+  filter: blur(${() => getRandomValue(30, 50)}px);
+  border-radius: 1000px;
+  border: 1px solid magenta;
+
+  position: absolute;
+  top: ${() => Math.floor(Math.random() * 100)}%;
+  left: ${() => Math.floor(Math.random() * 100)}%;
+
+  animation: ${() => cloudAnimation(10)} ${getRandomValue(9, 12)}s alternate
+    infinite;
+`;
