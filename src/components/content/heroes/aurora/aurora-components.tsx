6151c29c0584265c181c7edf9a572671abc77000
noasanto <n@bensanto.com>
2023-07-17 17:29:30 +0200

    chore: moved rainy flares and rainy color and rain effect to own component

--- src/components/content/heroes/aurora/aurora-components.tsx
+++ src/components/content/heroes/aurora/aurora-components.tsx
@@ -6,6 +6,7 @@ import PrecipitationEffect, { PrecipitationType } from './precipitation-effect';
 import React from 'react';
 import { DefaultFlares } from './default-flares';
 import { AuroraSun } from './sun';
+import { Rain } from './rain';
 
 const BACKGROUND_LAYER_Z = -2;
 const FOREGROUND_LAYER_Z = -1;
@@ -57,15 +58,6 @@ export const AuroraContainer = styled.div`
   pointer-events: none;
 `;
 
-export const AuroraRainyFlareColor = styled.div`
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(180deg, #231f67 0%, rgba(77, 121, 255, 0.27) 90%);
-  position: absolute;
-`;
-
 export const AuroraSnowyFlareColor = styled.div`
   top: 0;
   left: 0;
@@ -104,43 +96,7 @@ export const flaresByWeather: { [key in WeatherType] } = {
     </>
   ),
   [WeatherType.Cloudy]: <Clouds amount={20} />,
-  [WeatherType.Rainy]: (
-    <>
-      <AuroraRainyFlareColor />
-      <Flare
-        noAnimation={true}
-        size={970}
-        blur={125}
-        background={
-          'linear-gradient(180deg, #4d79ff 0%, rgba(77, 121, 255, 0.15) 100%)'
-        }
-        x={'38vw'}
-        y={'25vh'}
-      />
-      <Flare
-        noAnimation={true}
-        size={970}
-        background={'linear-gradient(180deg, #2a275f 0%, #535364 100%)'}
-        blur={125}
-        x={'30vw'}
-        y={'15vh'}
-      />
-      <Flare
-        noAnimation={true}
-        size={1094}
-        background={`linear-gradient(180deg, #150b28 0%, rgba(35, 33, 131, 0.15) 100%)`}
-        blur={125}
-        x={'64vw'}
-        y={'64vh'}
-      />
-      <PrecipitationEffect
-        dropCount={550}
-        speed={1}
-        type={PrecipitationType.Rain}
-        speedDeviation={0.1}
-      />
-    </>
-  ),
+  [WeatherType.Rainy]: <Rain amount={550} />,
   [WeatherType.Snowy]: (
     <>
       <AuroraSnowyFlareColor />
---
+++ src/components/content/heroes/aurora/rain.tsx
@@ -0,0 +1,52 @@
+import { Flare } from './flare';
+import PrecipitationEffect, { PrecipitationType } from './precipitation-effect';
+import React from 'react';
+import styled from 'styled-components';
+
+export const AuroraRainyFlareColor = styled.div`
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(180deg, #231f67 0%, rgba(77, 121, 255, 0.27) 90%);
+  position: absolute;
+`;
+export const Rain = (amount) => {
+  return (
+    <>
+      <AuroraRainyFlareColor />
+      <Flare
+        noAnimation={true}
+        size={970}
+        blur={125}
+        background={
+          'linear-gradient(180deg, #4d79ff 0%, rgba(77, 121, 255, 0.15) 100%)'
+        }
+        x={'38vw'}
+        y={'25vh'}
+      />
+      <Flare
+        noAnimation={true}
+        size={970}
+        background={'linear-gradient(180deg, #2a275f 0%, #535364 100%)'}
+        blur={125}
+        x={'30vw'}
+        y={'15vh'}
+      />
+      <Flare
+        noAnimation={true}
+        size={1094}
+        background={`linear-gradient(180deg, #150b28 0%, rgba(35, 33, 131, 0.15) 100%)`}
+        blur={125}
+        x={'64vw'}
+        y={'64vh'}
+      />
+      <PrecipitationEffect
+        dropCount={amount}
+        speed={1}
+        type={PrecipitationType.Rain}
+        speedDeviation={0.1}
+      />
+    </>
+  );
+};
