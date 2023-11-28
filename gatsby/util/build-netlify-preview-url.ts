714b21acd0baff6959ad9d4b928edb6702724d17
Jonathan Ostertag <jonathan.ostertag@satellytes.com>
2023-11-28 12:36:53 +0100

    chore: remove comment

--- gatsby/util/build-netlify-preview-url.ts
+++ gatsby/util/build-netlify-preview-url.ts
@@ -1,12 +1,3 @@
-/**
- * Get the Netlify Preview URL by previewId
- * (this equals the github entity id, e.g. PR id).
- * If no previewId is given the fallback URL gets return.
- *
- * On netlify the deployment urls follow a strict pattern:
- * - https://deploy-preview-${PREVIEW_ID}--${DOMAIN_NAME}.netlify.app/
- */
-
 const PRODUCTION_BRANCH = 'main';
 export const buildNetlifyPreviewUrl = ({ domainName, reviewId, branch }) => {
   if (!domainName || !reviewId || branch === PRODUCTION_BRANCH) {
