e48ad84251e69ee9b5af796cab5dc7297643ae79
noa.santo <noa.santo@satellytes.com>
2023-12-05 17:23:26 +0100

    feat: changed title of social links in footer, so it is clear that a new tab will open

--- src/components/layout/navigation/navigation.tsx
+++ src/components/layout/navigation/navigation.tsx
@@ -35,8 +35,7 @@ const NavigationBackground = styled.div`
 const LegalLinks = styled.ul`
   list-style-type: none;
   align-self: end;
-  margin: 0;
-  margin-top: 24px;
+  margin: 24px 0 0;
   padding: 0;
 
   display: flex;
@@ -212,7 +211,7 @@ const Navigation: React.FC<NavigationProps> = ({
             target="_blank"
             rel="noopener noreferrer"
             href="https://www.linkedin.com/company/satellytes"
-            title="Go to the Satellytes LinkedIn profile"
+            title="Open Satellytes LinkedIn profile in a new tab"
           >
             <IconWrapper>
               <Icon show="linked_in" />
@@ -221,7 +220,7 @@ const Navigation: React.FC<NavigationProps> = ({
         </SocialLinkItem>
         <SocialLinkItem>
           <SocialLink
-            title="Go to the Satellytes Github profile"
+            title="Open Satellytes GitHub profile in a new tab"
             target="_blank"
             rel="noopener noreferrer"
             href="https://github.com/satellytes"
