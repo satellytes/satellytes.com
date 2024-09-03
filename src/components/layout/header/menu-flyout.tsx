3a6d7f66b42b882475e2fb439ade99113ec23313
noa.santo <noa.santo@satellytes.com>
2024-09-03 16:45:38 +0200

    fix (a11y): add aria-hidden to menu flyout when visually hidden

--- src/components/layout/header/menu-flyout.tsx
+++ src/components/layout/header/menu-flyout.tsx
@@ -65,7 +65,11 @@ interface NavigationFlyoutProp {
 export const NavigationFlyout: React.FC<NavigationFlyoutProp> = (props) => {
   return (
     <>
-      <FullscreenOverlay $visible={props.visible} onClick={props.onClick}>
+      <FullscreenOverlay
+        $visible={props.visible}
+        onClick={props.onClick}
+        aria-hidden={!props.visible}
+      >
         <ScrollContainer>
           <FullHeightNavigation
             translation={props.translation}
@@ -77,6 +81,7 @@ export const NavigationFlyout: React.FC<NavigationFlyoutProp> = (props) => {
       <BackgroundOverlay
         $visible={props.visible}
         onClick={() => props.setIsNavigationVisible(false)}
+        aria-hidden={!props.visible}
       />
     </>
   );
