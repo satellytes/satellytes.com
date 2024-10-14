08869df21117b9f87bdb0b1483cb3af6d459d435
ipeke94 <26484292+ipeke94@users.noreply.github.com>
2024-10-14 22:45:04 +0200

    feat: testing leadinfo analytics functionality

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import 'vanilla-cookieconsent/dist/cookieconsent.css';
 import * as CookieConsent from 'vanilla-cookieconsent';
 import LeadinfoScript from '../components/layout/leadinfo-script';
@@ -7,7 +7,6 @@ import { getCookieConsentLocale } from '../components/i18n-helpers';
 
 const CookieConsentComponentV3 = () => {
   const { t, i18n } = useTranslation();
-  const [analyticsEnabled, setAnalyticsEnabled] = useState(false);
 
   useEffect(() => {
     CookieConsent.run({
@@ -23,37 +22,10 @@ const CookieConsentComponentV3 = () => {
           de: getCookieConsentLocale(i18n.language),
         },
       },
-      onFirstConsent: (param) => {
-        handleConsent(param.cookie.categories);
-      },
     });
   }, [t]);
 
-  const handleConsent = (categories) => {
-    const isAnalyticsAccepted = categories.includes('analytics');
-
-    if (isAnalyticsAccepted) {
-      setAnalyticsEnabled(true);
-      setLeadinfoCookies();
-    } else {
-      setAnalyticsEnabled(false);
-      clearLeadinfoCookies();
-    }
-  };
-
-  const setLeadinfoCookies = () => {
-    const twoYears = 63072000;
-    const sessionValue = new Date().toISOString();
-    document.cookie = `_li_id=some_value; max-age=${twoYears}; path=/`;
-    document.cookie = `_li_ses=${sessionValue}; max-age=0; path=/`;
-  };
-
-  const clearLeadinfoCookies = () => {
-    document.cookie = '_li_id=; max-age=0; path=/';
-    document.cookie = '_li_ses=; max-age=0; path=/';
-  };
-
-  return <LeadinfoScript enable={analyticsEnabled} />;
+  return <LeadinfoScript />;
 };
 
 export default CookieConsentComponentV3;
--- src/components/layout/leadinfo-script.tsx
+++ src/components/layout/leadinfo-script.tsx
@@ -1,30 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-let leadinfoInitialized = false;
-
-const LeadinfoScript = ({ enable }: { enable: boolean }) => {
-  const [script, setScript] = useState<HTMLScriptElement | null>(null);
-
+const LeadinfoScript = () => {
   useEffect(() => {
-    if (enable && !leadinfoInitialized) {
-      const leadinfoScript = document.createElement('script');
-      leadinfoScript.innerHTML = `
-        (function(l,e,a,d,i,n,f,o){if(!l[i]){l.GlobalLeadinfoNamespace=l.GlobalLeadinfoNamespace||[];
-          l.GlobalLeadinfoNamespace.push(i);l[i]=function(){(l[i].q=l[i].q||[]).push(arguments)};l[i].t=l[i].t||n;
-          l[i].q=l[i].q||[];o=e.createElement(a);f=e.getElementsByTagName(a)[0];o.async=1;o.src=d;f.parentNode.insertBefore(o,f);}
-          }(window,document,'script','https://cdn.leadinfo.eu/ping.js','leadinfo','LI-66D184CC97CFD'));
-      `;
-      document.head.appendChild(leadinfoScript);
-      setScript(leadinfoScript);
-      leadinfoInitialized = true;
-    }
+    const leadinfoScript = document.createElement('script');
+    leadinfoScript.setAttribute('type', 'text/plain');
+    leadinfoScript.setAttribute('data-category', 'analytics');
 
-    if (!enable && leadinfoInitialized && script) {
-      document.head.removeChild(script);
-      setScript(null);
-      leadinfoInitialized = false;
-    }
-  }, [enable, script]);
+    leadinfoScript.innerHTML = `
+      (function(l,e,a,d,i,n,f,o){if(!l[i]){l.GlobalLeadinfoNamespace=l.GlobalLeadinfoNamespace||[];
+        l.GlobalLeadinfoNamespace.push(i);l[i]=function(){(l[i].q=l[i].q||[]).push(arguments)};l[i].t=l[i].t||n;
+        l[i].q=l[i].q||[];o=e.createElement(a);f=e.getElementsByTagName(a)[0];o.async=1;o.src=d;f.parentNode.insertBefore(o,f);}
+        }(window,document,'script','https://cdn.leadinfo.eu/ping.js','leadinfo','LI-66D184CC97CFD'));
+    `;
+
+    document.head.appendChild(leadinfoScript);
+
+    return () => {
+      const existingScript = document.querySelector(
+        'script[data-category="analytics"]',
+      );
+      if (existingScript) {
+        document.head.removeChild(existingScript);
+      }
+    };
+  }, []);
 
   return null;
 };
