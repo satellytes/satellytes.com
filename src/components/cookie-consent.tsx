c07d0550b900965907afb2cab0663fdea4d2c120
noa.santo <noa.santo@satellytes.com>
2024-10-30 17:16:34 +0100

    feat: move youtube consent into cookie consent using iframe manager

--- src/components/cookie-consent.tsx
+++ src/components/cookie-consent.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import '@orestbida/iframemanager/dist/iframeManager.css';
+import '@orestbida/iframemanager/dist/iframemanager.js';
 import 'vanilla-cookieconsent/dist/cookieconsent.css';
 import * as CookieConsentLib from 'vanilla-cookieconsent';
 import enCookieConsentTranslations from '../assets/locales/en/cookie-consent-translations.json';
@@ -7,6 +9,51 @@ import { useTranslation } from 'react-i18next';
 
 export const CookieConsent = () => {
   const { i18n } = useTranslation();
+  // @ts-expect-error - iframemanager is defined at runtime
+  const im = iframemanager();
+
+  React.useEffect(() => {
+    im.run({
+      currLang: i18n.language,
+      onChange: ({ changedServices, eventSource }) => {
+        if (eventSource.type === 'click') {
+          const servicesToAccept = [
+            ...CookieConsentLib.getUserPreferences().acceptedServices[
+              'analytics'
+            ],
+            ...changedServices,
+          ];
+
+          CookieConsentLib.acceptService(servicesToAccept, 'analytics');
+        }
+      },
+
+      services: {
+        youtube: {
+          embedUrl: 'https://www.youtube-nocookie.com/embed/{data-id}',
+          thumbnailUrl: 'https://i3.ytimg.com/vi/{data-id}/hqdefault.jpg',
+          iframe: {
+            allow:
+              'accelerometer; encrypted-media; gyroscope; picture-in-picture; fullscreen;',
+          },
+          languages: {
+            en: {
+              notice:
+                'This content is hosted by a third party. By showing the external content you accept the <a rel="noreferrer noopener" href="https://www.youtube.com/t/terms" target="_blank">terms and conditions</a> of youtube.com.',
+              loadBtn: 'Load video',
+              loadAllBtn: "Load video and don't ask again",
+            },
+            de: {
+              notice:
+                'Dieser Inhalt wird von einer externen Quelle gehostet. Durch das Laden des Inhalts akzeptieren Sie die <a rel="noreferrer noopener" href="https://www.youtube.com/t/terms" target="_blank">Nutzungsbedingungen</a> von youtube.com.',
+              loadBtn: 'Video laden',
+              loadAllBtn: 'Video laden und nicht mehr fragen',
+            }, // TODO: Move translations to i18n
+          },
+        },
+      },
+    });
+  }, [i18n.language]);
 
   React.useEffect(() => {
     CookieConsentLib.run({
@@ -15,8 +62,8 @@ export const CookieConsent = () => {
           services: {
             youtube: {
               label: 'YouTube Embeds',
-              onAccept: () => console.log('youtube accepted'),
-              onReject: () => console.log('youtube rejected'),
+              onAccept: () => im.acceptService('youtube'),
+              onReject: () => im.rejectService('youtube'),
             },
           },
         },
--- src/components/legacy/markdown/youtube-embed.tsx
+++ src/components/legacy/markdown/youtube-embed.tsx
@@ -1,23 +1,9 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { theme } from '../../layout/theme';
-import { Button } from '../../ui/buttons/button';
-import { TextStyles } from '../../typography';
-import YouTubeConsentContext from '../../../context/youtube-consent-context';
-import { Link } from '../links/links';
 
 interface YoutubeEmbedProps {
   videoId: string;
 }
-
-// how to make iframe responsive: https://stackoverflow.com/questions/17838607/making-an-iframe-responsive
-const StyledIframe = styled.iframe`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`;
 
 const YoutubeEmbedWrapper = styled.div`
   position: relative;
@@ -26,71 +12,17 @@ const YoutubeEmbedWrapper = styled.div`
   height: 0;
 `;
 
-const YouTubePrivacyBannerWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  padding: 16px;
-  gap: 16px;
-  background-color: ${theme.palette.background.card};
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const LegalText = styled.p`
-  ${TextStyles.label}
-  text-align: center;
-`;
-
-const StyledButton = styled(Button)`
-  width: fit-content;
-`;
-
-const SyledLink = styled(Link)`
-  color: ${theme.palette.text.link.default};
-
-  &:hover {
-    border-bottom: 1px solid ${theme.palette.text.link.default};
-  }
-`;
-
 export const YoutubeEmbed = ({ videoId }: YoutubeEmbedProps) => {
-  const { consentGiven, giveConsent } = useContext(YouTubeConsentContext);
-
   return (
     <YoutubeEmbedWrapper>
-      {consentGiven ? (
-        <StyledIframe
-          title="YouTube video player"
-          width="560"
-          height="315"
-          src={`https://www.youtube-nocookie.com/embed/${videoId}`}
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></StyledIframe>
-      ) : (
-        <YouTubePrivacyBanner giveConsent={giveConsent} />
-      )}
+      <div
+        data-service="youtube"
+        data-id={videoId}
+        data-autoscale=""
+        data-iframe-id={`youtube-${videoId}`}
+        data-iframe-loading="lazy"
+        data-iframe-frameborder="0"
+      />
     </YoutubeEmbedWrapper>
-  );
-};
-
-const YouTubePrivacyBanner = ({ giveConsent }) => {
-  return (
-    <YouTubePrivacyBannerWrapper>
-      <LegalText>
-        By clicking on &quot;Show YouTube Content&quot; you accept{' '}
-        <SyledLink to={'https://policies.google.com/privacy?hl=en'}>
-          YouTube&apos;s privacy policy
-        </SyledLink>
-        .
-      </LegalText>
-      <StyledButton onClick={giveConsent}>Show YouTube Content</StyledButton>
-    </YouTubePrivacyBannerWrapper>
   );
 };
