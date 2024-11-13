0d5b11d35952eabcc50b6d6fb798b0ea324a19e1
noa.santo <noa.santo@satellytes.com>
2024-11-13 17:30:39 +0100

    fix: remove warnings that divs are in paragraphs by making everything a div

--- src/components/legacy/typography.tsx
+++ src/components/legacy/typography.tsx
@@ -27,7 +27,7 @@ export const SmallTitle = styled.h5`
  * Text
  *
  */
-export const Text = styled.p`
+export const Text = styled.div`
   ${TextStyles.textSR}
   ${up('md')} {
     ${TextStyles.textR}
